09cb4b621a7e708937b2e8e7053a65876ed626ee
Julien Dubois <jdubois@ippon.fr>
2012-04-06 11:55:57 +0200

    Moved JavaScript init function to the "assets"

--- src/main/webapp/assets/js/tatami.js
+++ src/main/webapp/assets/js/tatami.js
@@ -11,6 +11,62 @@ var userrefURL = '<a href="#" style="text-decoration:none" onclick="listUserTwee
 var tagrefREG = new RegExp("#(\\w+)", "g");
 var tagrefURL = '<a href="#" style="text-decoration:none" onclick="listTagTweets(\'$1\')" title="Show $1 related tweets"><em>#$1</em></a>';
 
+function initTatami() {
+    // left panel
+    $('#homeTabContent').load('/assets/fragments/home.html', function () {
+        refreshProfile();
+        $('#tweetContent').popover({
+            trigger: 'manual',
+            placement: 'bottom',
+            title: 'Error',
+            content: '<i class="icon-exclamation-sign"></i>&nbsp;Please type a message to tweet.'
+        });
+    });
+    $('#profileTabContent').load('/assets/fragments/profile.html');
+    $('#followUserContent').load('/assets/fragments/followUser.html', whoToFollow());
+    // auto-refresh
+    $('a[data-toggle="pill"]').on('show', function(e) {
+        if (e.target.hash == '#homeTabContent') {
+            refreshProfile();
+        } else if (e.target.hash == '#profileTabContent') {
+            displayProfile();
+        }
+    });
+
+    // right panel
+    $('#timeLinePanel').load('/assets/fragments/timeline.html', function() {
+        listTweets(true);
+    });
+    // browser's refresh shortcut override
+    shortcut.add("Ctrl+R", function() {
+        listTweets(true);
+    });
+    // infinite scroll
+    $(window).scroll(function() {
+        if ($('#timeline').is(':visible') && $(window).scrollTop() >= $(document).height() - $(window).height()) {
+            listTweets(false);
+        }
+    });
+
+    $('#favLinePanel').load('/assets/fragments/favline.html');
+    $('#userLinePanel').load('/assets/fragments/userline.html');
+    $('#tagLinePanel').load('/assets/fragments/tagline.html');
+
+    $('#piechartPanel').load('/assets/fragments/piechart.html');
+    $('#punchchartPanel').load('/assets/fragments/punchchart.html');
+    // auto-refresh
+    $('a[data-toggle="tab"]').on('show', function(e) {
+        if (e.target.hash == '#favLinePanel') {
+            listFavoriteTweets();
+        } else if (e.target.hash == '#piechartPanel') {
+            refreshPieChart();
+        } else if (e.target.hash == '#punchchartPanel') {
+            refreshPunchChart();
+        }
+    });
+}
+
+
 function tweet() {
     var $src = $('#tweetContent');
 	if(validateTweetContent($src) && validateXSS($src)){
