bdb5c0b0c95087095fbe697b61ff658f6b3a709a
David Martin <dmartin@ippon.fr>
2012-05-04 11:37:59 +0200

    Appliction configuration refactoring

--- src/main/java/fr/ippon/tatami/config/ApplicationConfiguration.java
+++ src/main/java/fr/ippon/tatami/config/ApplicationConfiguration.java
@@ -3,37 +3,26 @@ package fr.ippon.tatami.config;
 import java.io.IOException;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.thrift.transport.TTransportException;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 import fr.ippon.tatami.config.elasticsearch.ElasticSearchConfiguration;
 
 @Configuration
 @PropertySource("classpath:/META-INF/tatami/tatami.properties")
 @ComponentScan(basePackages = {"fr.ippon.tatami.application", "fr.ippon.tatami.repository", "fr.ippon.tatami.service", "fr.ippon.tatami.security"})
-@Import(value = {CacheConfiguration.class, CassandraConfiguration.class, ElasticSearchConfiguration.class})
+@Import(value = {MiscBeanConfiguration.class, CacheConfiguration.class, CassandraConfiguration.class, ElasticSearchConfiguration.class})
 @ImportResource({"classpath:META-INF/spring/applicationContext-security.xml"})
 public class ApplicationConfiguration {
 
     private final Log log = LogFactory.getLog(ApplicationConfiguration.class);
-
-    @Inject
-    private Environment env;
-
-    @Bean
-    public String shortURLPrefix() {
-        return this.env.getProperty("tatami.short.url.prefix");
-    }
 
     @PostConstruct
     public void initTatami() throws IOException, TTransportException {
--- src/test/java/fr/ippon/tatami/application/ApplicationTestConfiguration.java
+++ src/test/java/fr/ippon/tatami/application/ApplicationTestConfiguration.java
@@ -1,19 +1,26 @@
 package fr.ippon.tatami.application;
 
-import fr.ippon.tatami.config.CassandraConfiguration;
-import fr.ippon.tatami.config.elasticsearch.ElasticSearchConfiguration;
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.thrift.transport.TTransportException;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
+import fr.ippon.tatami.config.CassandraConfiguration;
+import fr.ippon.tatami.config.MiscBeanConfiguration;
+import fr.ippon.tatami.config.elasticsearch.ElasticSearchConfiguration;
 
 @Configuration
 @PropertySource("classpath:/tatami/tatami-test.properties")
 @ComponentScan(basePackages = {"fr.ippon.tatami.repository", "fr.ippon.tatami.service", "fr.ippon.tatami.security"})
-@Import(value = {CassandraConfiguration.class, ElasticSearchConfiguration.class})
+@Import(value = {MiscBeanConfiguration.class, CassandraConfiguration.class, ElasticSearchConfiguration.class})
 @ImportResource({"classpath:META-INF/spring/applicationContext-security.xml"})
 public class ApplicationTestConfiguration {
 
@@ -21,6 +28,6 @@ public class ApplicationTestConfiguration {
 
     @PostConstruct
     public void initTatami() throws IOException, TTransportException {
-        log.info("Tatami test context started!");
+        this.log.info("Tatami test context started!");
     }
 }
