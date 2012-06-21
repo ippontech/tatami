3be9041cfb239113d91e299cd5000f8b0f7892a3
Julien Dubois <jdubois@ippon.fr>
2012-06-22 00:20:16 +0200

    Updated documentation

--- src/main/java/fr/ippon/tatami/repository/DaylineRepository.java
+++ src/main/java/fr/ippon/tatami/repository/DaylineRepository.java
@@ -18,7 +18,7 @@ public interface DaylineRepository {
     void addStatusToDayline(Status status, String domain, String day);
 
     /**
-     * Get the statistics for one day, in the form &lt;login, number of tweets&gt;.
+     * Get the statistics for one day, in the form &lt;username, number of status updates&gt;.
      */
     Collection<UserStatusStat> getDayline(String domain, String day);
 }
