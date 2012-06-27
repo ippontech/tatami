9342dd9e21beb1ddbd632de31cc80c0083a8f3b1
Julien Dubois <julien.dubois@gmail.com>
2012-06-27 11:43:09 +0300

    Update master

--- src/main/java/fr/ippon/tatami/web/controller/AccountPasswordController.java
+++ src/main/java/fr/ippon/tatami/web/controller/AccountPasswordController.java
@@ -83,7 +83,6 @@ public class AccountPasswordController {
             }
             ModelAndView mv = new ModelAndView("account/password");
             mv.addAllObjects(result.getModel());
-            log.debug("mv:="+mv);
             return mv;
         }
 
