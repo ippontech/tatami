6b1777329c15bc84f9aea9d7a3dd378ecd13a552
Julien Dubois <jdubois@ippon.fr>
2012-07-24 12:29:57 +0200

    Created a Spring Security "admin" role

--- src/main/java/fr/ippon/tatami/security/TatamiUserDetailsService.java
+++ src/main/java/fr/ippon/tatami/security/TatamiUserDetailsService.java
@@ -4,6 +4,7 @@ import fr.ippon.tatami.domain.User;
 import fr.ippon.tatami.service.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 /**
@@ -26,15 +28,37 @@ public class TatamiUserDetailsService implements UserDetailsService {
 
     private final Log log = LogFactory.getLog(TatamiUserDetailsService.class);
 
-    private Collection<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+    private Collection<GrantedAuthority> userGrantedAuthorities = new ArrayList<GrantedAuthority>();
+
+    private Collection<GrantedAuthority> adminGrantedAuthorities = new ArrayList<GrantedAuthority>();
+
+    private Collection<String> adminUsers = null;
 
     @Inject
     private UserService userService;
 
+    @Inject
+    Environment env;
+
     @PostConstruct
     public void init() {
+        //Roles for "normal" users
         GrantedAuthority roleUser = new SimpleGrantedAuthority("ROLE_USER");
-        grantedAuthorities.add(roleUser);
+        userGrantedAuthorities.add(roleUser);
+
+        //Roles for "admin" users, configured in tatami.properties
+        GrantedAuthority roleAdmin = new SimpleGrantedAuthority("ROLE_ADMIN");
+        adminGrantedAuthorities.add(roleUser);
+        adminGrantedAuthorities.add(roleAdmin);
+
+        String adminUsersList = this.env.getProperty("tatami.admin.users");
+        String[] adminUsersArray = adminUsersList.split(",");
+        adminUsers = new ArrayList<String>(Arrays.asList(adminUsersArray));
+        if (log.isDebugEnabled()) {
+            for (String admin : adminUsers) {
+                log.debug("User \"" + admin + "\" is an administrator.");
+            }
+        }
     }
 
     @Override
@@ -46,10 +70,18 @@ public class TatamiUserDetailsService implements UserDetailsService {
         if (userFromCassandra == null) {
             throw new UsernameNotFoundException("User " + username + " was not found in Cassandra");
         }
-        org.springframework.security.core.userdetails.User springSecurityUser =
-                new org.springframework.security.core.userdetails.User(username, userFromCassandra.getPassword(),
-                        grantedAuthorities);
+        org.springframework.security.core.userdetails.User springSecurityUser = null;
 
+        if (adminUsers.contains(username)) {
+            if (log.isDebugEnabled()) {
+                log.debug("User \"" + username + "\" is an administrator.");
+            }
+            springSecurityUser = new org.springframework.security.core.userdetails.User(username, userFromCassandra.getPassword(),
+                    adminGrantedAuthorities);
+        } else {
+            springSecurityUser = new org.springframework.security.core.userdetails.User(username, userFromCassandra.getPassword(),
+                        userGrantedAuthorities);
+        }
         return springSecurityUser;
     }
 }
