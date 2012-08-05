19e3fc78d8e23c2cd9900308afb439cef233ca94
Fabien Arrault <farrault@ippon.fr>
2012-08-05 19:34:06 +0200

    UITesting : google auth

--- src/integration/java/fr/ippon/tatami/uitest/AuthenticationSpec.groovy
+++ src/integration/java/fr/ippon/tatami/uitest/AuthenticationSpec.groovy
@@ -1,9 +1,13 @@
 package fr.ippon.tatami.uitest
 
+import org.junit.Assume;
+
 import geb.Page
 import geb.spock.GebSpec
 
 import pages.*
+import pages.google.GoogleAuthenticationPage;
+import pages.google.GoogleOpenIdPage;
 
 class AuthenticationSpec extends TatamiBaseGebSpec {
 		
@@ -12,10 +16,10 @@ class AuthenticationSpec extends TatamiBaseGebSpec {
 		// TODO : il doit y avoir un moyen plus simple/propre de choisir le driver : (cf GebConfig aussi)
 		// by default we use HtmlUnit (cf GebConfig.groovy )
 		
-//		if(!System.getProperty('gev.env')) {
-////			System.setProperty('geb.env',"chrome")
-//			System.setProperty('geb.env',"firefox")
-//		}
+		if(!System.getProperty('gev.env')) {
+//			System.setProperty('geb.env',"chrome")
+			System.setProperty('geb.env',"firefox")
+		}
 	}
 	
     def "login as admin with ldap"() {
@@ -33,11 +37,10 @@ class AuthenticationSpec extends TatamiBaseGebSpec {
         loginButton.click()
 		
         then:
-        at HomePage
+        waitFor { at HomePage }
 		
 		and:
-		assert dropDownMenu
-		assert adminLink
+		adminLink.isPresent();
 		
     }
 	
@@ -58,10 +61,10 @@ class AuthenticationSpec extends TatamiBaseGebSpec {
 		loginButton.click()
 		 
 		then:
-		at HomePage
+		waitFor { at HomePage }
 		
 		and:
-		assert ! adminLink
+		! adminLink.isPresent()
 		
 		and:
 		if(realBrowser()) {
@@ -72,30 +75,34 @@ class AuthenticationSpec extends TatamiBaseGebSpec {
 	
 	// TODO : test new openid user can login ... (auto-registration)
 	
-//	def "login as normal user with google"() {
-//		given:
-//		to LoginPage
-//		verifyAt()
-//		
-//		// assert user not in database ?
-//		
-//		when:
-//		googleForm.with {
-//			j_username = "farrault@ippon.fr"
-//		}
-//		 
-//		and:
-//		googleButton.click()
-//		
-//		then :
-//		// TODO : login on google ?
-//		// TODO : authorize localhost on google ?
-//		 
-//		then:
-//		at HomePage
-//		
-//		and:
-//		assert ! adminLink
-//	
-//	}
+	def "login as normal user with google"() {
+		given:
+		to LoginPage
+		verifyAt()
+		// and google account not accepting localhost ...
+		// assert user not in database ?
+				
+		when:
+		googleButton.click()
+	
+		// Login at google :
+		waitFor { at GoogleAuthenticationPage }
+		loginForm.with {
+			Email = "farrault@ippon.fr"
+			Passwd = System.getProperty("mypassord");
+		}
+		loginButton.click()
+		
+		// Authorize localhost on google :
+		waitFor { at GoogleOpenIdPage } 
+		rememberChoicesCB.value(false)
+		approveButton.click()
+		 
+		then:
+		at HomePage
+		
+		and:
+		! adminLink.isPresent()
+	
+	}
 }
--- src/integration/java/pages/HomePage.groovy
+++ src/integration/java/pages/HomePage.groovy
@@ -2,19 +2,19 @@ package pages
 
 import geb.Page;
 
-class HomePage extends Page {
+class HomePage extends TatamiBasePage {
     static url = "tatami/"
  
-    static at = { /* Thread.sleep(10);*/ profileContent != null } // sometimes I got an error here ?!
+    static at = { profileContent != null } // sometimes I got an error here ?!
  
     static content = {
-      profileContent { $("div#profileContent") }
-	  // les contenus ci-dessous ne sont accessibles qu'avec le javascript puisque les div sont chargés vides ...
-      updateStatus { $("input",value:"Update your status") }
-	  dropDownMenu { $("ul.dropdown-menu") } // <ul class="dropdown-menu">
-	  adminLink(required: false) {
-		  // <a href="/tatami/admin"> // href returns absolute url so we need to filter with endsWith ...
-		  dropDownMenu.find("a",href:endsWith('/tatami/admin')) 
-	  }
+		// défini dans la classe mère :
+//		dropDownMenu
+//    	adminLink
+
+		profileContent { $("div#profileContent") }
+	  
+		// les contenus ci-dessous ne sont accessibles qu'avec le javascript puisque les div sont chargés vides ...
+		updateStatus { $("input",value:"Update your status") }
     }
 }
