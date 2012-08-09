33df413d1ca3ec4f2f2f491e8223abd3479c0065
Julien Dubois <jdubois@ippon.fr>
2012-08-09 15:31:45 +0200

    Removed old code used to generate weekly stats

--- src/main/java/fr/ippon/tatami/web/rest/StatsController.java
+++ src/main/java/fr/ippon/tatami/web/rest/StatsController.java
@@ -1,6 +1,5 @@
 package fr.ippon.tatami.web.rest;
 
-import fr.ippon.tatami.domain.DayStatusStat;
 import fr.ippon.tatami.domain.UserStatusStat;
 import fr.ippon.tatami.service.TimelineService;
 import org.apache.commons.logging.Log;
@@ -37,42 +36,5 @@ public class StatsController {
         log.debug("REST request to get the users stats.");
         Collection<UserStatusStat> statuses = timelineService.getDayline();
         return statuses;
-    }
-
-    /**
-     * GET  /stats/week -> statistics for this week
-     */
-    @RequestMapping(value = "/rest/stats/week",
-            method = RequestMethod.GET,
-            produces = "application/json")
-    @ResponseBody
-    public Collection<DayStatusStat> listWeekStatusStats() {
-        log.debug("REST request to get the users stats.");
-
-        Calendar date = Calendar.getInstance();    //TODO parameterized version
-        DayStatusStat stats[] = new DayStatusStat[7];
-        Set<String> users = new HashSet<String>();
-        for (int i = stats.length; i > 0; i--) {
-            date.add(Calendar.DATE, -1);    // let's analyze the past week
-
-            DayStatusStat dayStat = new DayStatusStat(date.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.ENGLISH));
-            log.debug("Scanning " + dayStat.getDay() + "...");
-            Collection<UserStatusStat> statuses = timelineService.getDayline(date.getTime());
-            dayStat.setStats(statuses);
-
-            stats[i - 1] = dayStat;    // oldest first
-        }
-        this.enforceUsers(stats, users);    // each day's users list has to be identical to the others
-        return Arrays.asList(stats);
-    }
-
-    private void enforceUsers(DayStatusStat stats[], Set<String> allUsers) {
-        for (DayStatusStat stat : stats) {
-            for (String login : allUsers) {
-                if (!stat.getStats().contains(new UserStatusStat(login, 0L))) {
-                    stat.getStats().add(new UserStatusStat(login, 0L));
-                }
-            }
-        }
     }
 }
