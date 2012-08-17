d5038032bcffe1e55533583edec85b2bdc736295
Julien Dubois <jdubois@ippon.fr>
2012-08-17 21:42:01 +0200

    Corrected sed script

--- etc/installation/ubuntu/uninstall.sh
+++ etc/installation/ubuntu/uninstall.sh
@@ -3,13 +3,16 @@
 # description: Uninstalls Tatami on Ubuntu
 # This script must be run by the "root" user.
 #
+# Run this script directly by typing :
+# ﻿curl -L https://github.com/ippontech/tatami/raw/master/etc/installation/ubuntu/uninstall.sh | sudo bash
+#
 # - Deletes the "/opt/tatami"
 # - Deletes the "tatami" user
 
 echo "Remove Tatami user"
 
 # Remove JNA settings
-sed '/tatami/d' /etc/security/limits.conf
+sed -i '/tatami/d' /etc/security/limits.conf
 
 userdel -f tatami
 
