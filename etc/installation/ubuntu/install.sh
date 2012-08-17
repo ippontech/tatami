92994845a246382bdddc303fb93f54b6c87c34b2
Julien Dubois <jdubois@ippon.fr>
2012-08-18 00:35:59 +0200

    Installation script update

--- etc/installation/ubuntu/install.sh
+++ etc/installation/ubuntu/install.sh
@@ -89,7 +89,7 @@ ln -s $TATAMI_DIR/maven/apache-maven-$MAVEN_VERSION $TATAMI_DIR/maven/current
 echo "# Begin tatami configuration" ﻿>> /home/tatami/.bashrc
 echo "export M2_HOME=/opt/tatami/maven/current" >> /home/tatami/.bashrc
 echo "export PATH=/opt/tatami/maven/current/bin:$PATH" ﻿>> /home/tatami/.bashrc
-echo "export MAVEN_OPTS=-XX:MaxPermSize=64m -Xms256m -Xmx512m" ﻿>> /home/tatami/.bashrc
+echo "export MAVEN_OPTS=\"-XX:MaxPermSize=64m -Xms256m -Xmx512m\"" ﻿>> /home/tatami/.bashrc
 echo "# End tatami configuration" ﻿>> /home/tatami/.bashrc
 
 # Configure Maven repository
