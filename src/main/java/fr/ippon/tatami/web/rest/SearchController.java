1c174409856df686435f844800d63a49a79dfb25
David Martin <dmartin@ippon.fr>
2012-09-17 17:12:10 +0200

    Still a XSS vulnerablity fixed with the search field

--- src/main/java/fr/ippon/tatami/web/rest/SearchController.java
+++ src/main/java/fr/ippon/tatami/web/rest/SearchController.java
@@ -1,5 +1,22 @@
 package fr.ippon.tatami.web.rest;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import fr.ippon.tatami.domain.SharedStatusInfo;
 import fr.ippon.tatami.domain.Status;
 import fr.ippon.tatami.domain.User;
@@ -7,19 +24,6 @@ import fr.ippon.tatami.security.AuthenticationService;
 import fr.ippon.tatami.service.SearchService;
 import fr.ippon.tatami.service.TimelineService;
 import fr.ippon.tatami.service.util.DomainUtil;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author dmartin
@@ -50,21 +54,21 @@ public class SearchController {
             produces = "application/json")
     @ResponseBody
     public Collection<Status> listStatusForUser(@RequestParam(value = "q", required = false, defaultValue = "") String q,
-                                                @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
-                                                @RequestParam(value = "rpp", required = false, defaultValue = "20") Integer rpp) {
+            @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+            @RequestParam(value = "rpp", required = false, defaultValue = "20") Integer rpp) {
 
-        if (log.isDebugEnabled()) {
-            log.debug("REST request to search status containing these words (" + q + ").");
+        if (this.log.isDebugEnabled()) {
+            this.log.debug("REST request to search status containing these words (" + q + ").");
         }
-        final User currentUser = authenticationService.getCurrentUser();
+        final User currentUser = this.authenticationService.getCurrentUser();
         String domain = DomainUtil.getDomainFromLogin(currentUser.getLogin());
         Map<String, SharedStatusInfo> line;
         if (q != null && !q.equals("")) {
-            line = searchService.searchStatus(domain, q, page, rpp);
+            line = this.searchService.searchStatus(domain, Jsoup.clean(q, Whitelist.none()), page, rpp);
         } else {
             line = new HashMap<String, SharedStatusInfo>();
         }
-        return timelineService.buildStatusList(line);
+        return this.timelineService.buildStatusList(line);
     }
 
 }
--- src/main/webapp/js/tatami-home.js
+++ src/main/webapp/js/tatami-home.js
@@ -702,13 +702,18 @@ app.View.SearchSearchView = Backbone.View.extend({
     var self = this;
 
     _.each($(this.el).serializeArray(), function(input){
-      if(input.name === 'search')
-        self.model.options.search = input.value;
+      if(input.name === 'search') {
+    	var escaped_value = this.escapeField(input.value + '');
+        self.model.options.search = escaped_value;
+      }
     });
 
     this.search();
   },
 
+  escapeField: function (value) {
+	  return value.replace(new RegExp('["\'<>]', 'gi'), '');
+  },
   search: function () {
     app.router.navigate('//search/' + this.model.options.search, {trigger: false,replace:false});
     this.fetch();
@@ -731,7 +736,7 @@ app.View.SearchSearchView = Backbone.View.extend({
   },
 
   render: function () {
-    var search = (typeof this.model.options.search === 'undefined')? '':this.model.options.search;
+    var search = this.escapeField((typeof this.model.options.search === 'undefined')? '':this.model.options.search);
     $(this.el).html(this.template({search: search}));
     return $(this.el);
   }
