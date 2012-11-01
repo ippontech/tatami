eab831f1f79d03f7fb9b99190172ad3e62ff1419
dygcao <dygcao@gmail.com>
2012-11-01 21:39:33 +0100

    Add unit test for repository : TagFollowerRepository

---
+++ src/test/java/fr/ippon/tatami/repository/TagFollowerRepositoryTest.java
@@ -0,0 +1,38 @@
+package fr.ippon.tatami.repository;
+
+import fr.ippon.tatami.AbstractCassandraTatamiTest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dygcao
+ * Date: 29/10/12
+ * Time: 21:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class TagFollowerRepositoryTest extends AbstractCassandraTatamiTest {
+
+    @Inject
+    public TagFollowerRepository tagFollowerRepository;
+
+           @Test
+           public void shouldGetAStatusRepositoryInjected() {
+                assertThat(tagFollowerRepository, notNullValue());
+           }
+
+           @Test
+           public void shouldAddFollower() {
+                   String domain = "ippon.fr";
+                   String login = "jdubois@ippon.fr";
+                   String tag = "jdubois";
+                   assertThat(tagFollowerRepository.addFollower(domain, login, tag), notNullValue());
+           }
+
+           @Test(expected = ValidationException.class)
+           public void shouldNotAddFollowerBecauseLoginNull() {
+                   String domain = "ippon.fr";
+                   String login = null;
+                   String tag = "jdubois";
+
+                   tagFollowerRepository.addFollower(domain,login, tag);
+           }
+}
