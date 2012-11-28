a04c0353e85a4a9d9170023da9b8a1aa40b491ac
hellsingblack <hellsingblack@gmail.com>
2012-11-28 12:20:04 +0100

    JS
    Fix bug js

--- src/main/webapp/js/tatami-search.js
+++ src/main/webapp/js/tatami-search.js
@@ -26,7 +26,7 @@ else {
 app.View.SearchFormHeaderView = Backbone.View.extend({
   initialize: function(){
   },
-
+  
   events: {
     'submit' : 'submit'
   },
@@ -40,19 +40,18 @@ app.View.SearchFormHeaderView = Backbone.View.extend({
       if(input.name === 'search')
         search = input.value;
     });
-    if(search)
-      window.location = '/tatami/#/search/' + search;
+    if(search){
+    	 window.location = '/tatami/#/search/' + search;
+    }
+     
   }
 });
 
-$(function() {
 
-  app.views.searchFromHeaderView = new app.View.SearchFormHeaderView({
-    el: $('#searchHeader')
-  });
-  
+var searchFromHeaderView = new app.View.SearchFormHeaderView({
+	el: $('#searchHeader') 
 });
-
+   
 
 app.View.switchSearchAgent = Backbone.View.extend({
 	initialize: function(){
