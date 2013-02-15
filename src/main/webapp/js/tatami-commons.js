2163982c25ab033957328b2a8c52573d260b1fd2
godu <arthur.weber@viacesi.fr>
2013-02-15 14:37:03 +0100

    Fix autocompletion error

--- src/main/webapp/js/tatami-commons.js
+++ src/main/webapp/js/tatami-commons.js
@@ -746,13 +746,13 @@ function Suggester(element) {
         var query = raw_query.substring(0, caretPosition);
         var matchLogin = query.match(patterns.login);
         var matchHash = query.match(patterns.hash);
-        if (typeof matchLogin === 'undefined' && typeof matchHash === 'undefined') {
+        if (matchLogin === null && matchHash === null) {
             if (this.shown) {
                 this.hide();
             }
             return;
         }
-        var query2 = (typeof matchLogin !== 'undefined') ? matchHash[0] : matchLogin[0];
+        var query2 = (matchLogin === null) ? matchHash[0] : matchLogin[0];
 
         // Didn't find a good reg ex that doesn't catch the character before # or @ : have to cut it down :
         query2 = (query2.charAt(0) != '#' && query2.charAt(0) != '@') ? query2.substring(1, query2.length) : query2;
