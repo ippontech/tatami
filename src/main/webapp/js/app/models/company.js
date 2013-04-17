40d58c6bd54288bf09c0cb0afacdd5c606e50c88
godu <arthur.weber@viacesi.fr>
2013-04-17 18:41:38 +0200

    Cleanup

--- src/main/webapp/js/app/models/company.js
+++
@@ -1,9 +0,0 @@
-(function(Backbone, Tatami){
-
-    var Company = Backbone.Model.extend({
-        urlRoot: '/tatami/rest/company'
-    });
-
-    Tatami.Models.Company = Company;
-
-})(Backbone, Tatami);
