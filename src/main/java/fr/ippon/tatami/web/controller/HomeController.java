e4f973e8ac98345a62a449c3d47504349df93908
godu <arthur.weber@viacesi.fr>
2013-04-22 15:03:23 +0200

    Add new routes : /new & /new/

--- src/main/java/fr/ippon/tatami/web/controller/HomeController.java
+++ src/main/java/fr/ippon/tatami/web/controller/HomeController.java
@@ -53,7 +53,7 @@ public class HomeController {
         return mv;
     }
 
-    @RequestMapping(value = { "/new/**" }, method = RequestMethod.GET)
+    @RequestMapping(value = { "/new/**", "/new", "/new/" }, method = RequestMethod.GET)
     public ModelAndView newUI(@RequestParam(required = false) String action, HttpServletRequest request) {
         ModelAndView mv = new ModelAndView("new");
         User currentUser = authenticationService.getCurrentUser();
