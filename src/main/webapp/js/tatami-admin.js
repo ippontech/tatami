559b3c52a5f3bbd828c360fd1d76d0cc9b09e778
Julien Dubois <jdubois@ippon.fr>
2013-06-21 16:35:32 +0200

    corrected group members

--- src/main/webapp/js/tatami-admin.js
+++ src/main/webapp/js/tatami-admin.js
@@ -657,6 +657,28 @@ app.View.Group = Backbone.View.extend({
     }
 });
 
+app.Model.ListUserGroupModel = Backbone.Model.extend({
+    idAttribute : 'username',
+    defaults : {
+        avatar : '',
+        firstName : '',
+        lastName : '',
+        role : ''
+    },
+    toJSON : function(){
+        return _.extend(Backbone.Model.prototype.toJSON.apply(this), {
+            avatar : (this.get('avatar'))? '/tatami/avatar/' + this.get('avatar') + '/photo.jpg': '/img/default_image_profile.png'
+        });
+    }
+});
+
+app.Collection.ListUserGroupCollection = Backbone.Collection.extend({
+    model : app.Model.ListUserGroupModel,
+    url : function() {
+        return '/tatami/rest/groups/' + this.options.groupId + '/members/';
+    }
+});
+
 app.View.ActionsGroup = Backbone.View.extend({
     tagName : 'td',
     template : {
