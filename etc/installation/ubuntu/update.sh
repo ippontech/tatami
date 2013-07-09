68ca6a3d86bc117e1f6610600a8e419ac278e62e
Julien Dubois <jdubois@ippon.fr>
2013-07-09 10:49:35 +0200

    updated update script for the new production server

--- etc/installation/ubuntu/update.sh
+++ etc/installation/ubuntu/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # description: Updates Tatami from Git
-# This script must be run by the "root" user.
+# This script must be run by the "tatami" user, who must be a sudoer.
 echo "Welcome to the Tatami updater"
 
 #################################
@@ -16,7 +16,7 @@ export TATAMI_DIR=/opt/tatami
 #################################
 cd $TATAMI_DIR/application/tatami
 git pull
-su - $USER -c "cd /opt/tatami/application/tatami && mvn -Ppreprod -DskipTests clean package"
-/etc/init.d/jetty stop
-cp /opt/tatami/application/tatami/target/root.war /opt/jetty/webapps/root.war
-/etc/init.d/jetty start
+cd /opt/tatami/application/tatami && mvn -Ppreprod -DskipTests clean package
+sudo /etc/init.d/jetty stop
+sudo cp /opt/tatami/application/tatami/target/root.war /opt/jetty/webapps/root.war
+sudo /etc/init.d/jetty start
