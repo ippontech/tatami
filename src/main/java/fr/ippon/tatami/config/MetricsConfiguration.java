6c057181671339b747bcf9642de8989a6e12ad77
Pierre-Alain RIVIERE <--global>
2014-03-09 21:56:54 +0100

    Fix graphite hostname checking

--- src/main/java/fr/ippon/tatami/config/MetricsConfiguration.java
+++ src/main/java/fr/ippon/tatami/config/MetricsConfiguration.java
@@ -2,17 +2,21 @@ package fr.ippon.tatami.config;
 
 import com.yammer.metrics.HealthChecks;
 import com.yammer.metrics.reporting.GraphiteReporter;
+
 import fr.ippon.tatami.config.metrics.CassandraHealthCheck;
 import fr.ippon.tatami.config.metrics.JavaMailHealthCheck;
 import fr.ippon.tatami.service.MailService;
 import me.prettyprint.hector.api.Keyspace;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -37,7 +41,7 @@ public class MetricsConfiguration {
             HealthChecks.register(new JavaMailHealthCheck(mailService));
 
             String graphiteHost = env.getProperty("tatami.metrics.graphite.host");
-            if (graphiteHost != null) {
+            if (!StringUtils.isEmpty(graphiteHost)) {
                 log.debug("Initializing Metrics Graphite reporting");
                 Integer graphitePort = env.getProperty("tatami.metrics.graphite.port", Integer.class);
                 GraphiteReporter.enable(1,
