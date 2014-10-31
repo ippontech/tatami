bbb12cbdcd23275db369640ef755cf69e0b4b464
kenny <kenny@localhost.localdomain>
2014-10-30 22:16:39 -0400

    Updating

--- src/main/webapp/app/tatamiapp.js
+++ src/main/webapp/app/tatamiapp.js
@@ -1,6 +1,6 @@
 var tatamiApp = angular.module('tatamiApp', ['ngResource', 'ui.bootstrap', 'angularFileUpload']);
 
-
+// Testing a new commit
 tatamiApp.config(['$resourceProvider', function ($resourceProvider) {
     // Don't strip trailing slashes from calculated URLs
     $resourceProvider.defaults.stripTrailingSlashes = false;
