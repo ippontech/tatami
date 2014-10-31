56ccf2205fc100d98ba4ac0180e8ad3b9fbbb441
Jeremy Narron <jnarron@users.noreply.github.com>
2014-10-31 09:08:26 -0400

    added back in config I accidentally removed

--- src/main/webapp/app/TatamiApp.js
+++ src/main/webapp/app/TatamiApp.js
@@ -1,1 +1,6 @@
 var TatamiApp = angular.module('TatamiApp', ['TimelineModule', 'TatamModule']);
+
+TatamiApp.config(['$resourceProvider', function ($resourceProvider) {
+    // Don't strip trailing slashes from calculated URLs
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
