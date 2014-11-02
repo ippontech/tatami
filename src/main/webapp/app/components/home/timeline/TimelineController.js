dca71beee4e195e1a24eac227f50e0e3254d234f
Jeremy Narron <jnarron@users.noreply.github.com>
2014-11-02 15:20:04 -0500

    attached timeline controller to timeline module

--- src/main/webapp/app/components/home/timeline/TimelineController.js
+++ src/main/webapp/app/components/home/timeline/TimelineController.js
@@ -1,4 +1,4 @@
-TatamiApp.controller('TimelineController', ['$scope', 'TimelineService', 'ProfileService', 
+TimelineModule.controller('TimelineController', ['$scope', 'TimelineService', 'ProfileService', 
     function($scope, TimelineService, ProfileService) {
         $scope.statuses = TimelineService.query();
         $scope.profile = ProfileService.get();
