7faa83a51876488f10460121f97aef623721292e
kenny <kenny@localhost.localdomain>
2014-11-03 14:27:53 -0500

    Upated TagsService to not have "popular=true" hardcoded, added ng-cloak to sidebar, and used a promise in getting avatarUrl

--- src/main/webapp/app/components/home/sidebar/UserController.js
+++ src/main/webapp/app/components/home/sidebar/UserController.js
@@ -1,3 +1,7 @@
 SidebarModule.controller('UserController',['$scope', 'ProfileService', function($scope, ProfileService) {
-	$scope.user = ProfileService.get();
+	$scope.user = ProfileService.get(function () {
+        $scope.avatarUrl = $scope.user.avatar=='' ? $scope.avartarUrl : '/tatami/avatar/' + $scope.user.avatar + '/photo.jpg';
+    }),
+    $scope.avatarUrl = '/img/default_image_profile.png';
+
 }]);
--- src/main/webapp/app/shared/services/TagService.js
+++ src/main/webapp/app/shared/services/TagService.js
@@ -1,4 +1,4 @@
 TatamiApp.factory('TagService', ['$resource', function($resource) {
-    return $resource('/tatami/rest/tags?popular=true');
+    return $resource('/tatami/rest/tags:isPop');
     // How to have parameters in the url like :popular (t/f)?
 }]);
