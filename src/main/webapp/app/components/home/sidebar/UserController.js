c31939fadf8de65981f3af5e87f5cab83b9f987d
Jeremy Narron <jnarron@users.noreply.github.com>
2014-11-03 10:38:34 -0500

    added reminder comment

--- src/main/webapp/app/components/home/sidebar/UserController.js
+++ src/main/webapp/app/components/home/sidebar/UserController.js
@@ -1,4 +1,10 @@
 SidebarModule.controller('UserController',['$scope', 'ProfileService', function($scope, ProfileService) {
-	$scope.user = ProfileService.get();
-    $scope.user.avatar = '';
+	$scope.user = ProfileService.get();   
+
+    /*
+     * quick fix for network error from angular requesting the wrong avatar
+     * url. need to implement $q/promise to stop angular from requesting the 
+     * default profile image even when it is not needed
+     */
+    $scope.user.avatar = ''; 
 }]);
