9c3dcef91fa76e726a0d9b2bc47533cf207d793d
Jeremy Narron <jnarron@users.noreply.github.com>
2014-11-04 15:21:21 -0500

    tab to 4 spaces

--- src/main/webapp/app/components/home/sidebar/UserController.js
+++ src/main/webapp/app/components/home/sidebar/UserController.js
@@ -1,6 +1,6 @@
 SidebarModule.controller('UserController', ['$scope', 'ProfileService', function($scope, ProfileService) {
     $scope.avatarUrl = '/img/default_image_profile.png',
-	$scope.user = ProfileService.get(function() {
+    $scope.user = ProfileService.get(function() {
         $scope.avatarUrl = $scope.user.avatar=='' ? $scope.avartarUrl : '/tatami/avatar/' + $scope.user.avatar + '/photo.jpg';
     })
 }]);
