5cec817d44b4538fcd7cbae8f8b4d9ab002c1a39
Jeremy Narron <jnarron@users.noreply.github.com>
2014-11-07 12:01:24 -0500

    added TopMenu

--- src/main/webapp/app/shared/configs/TranslateConfig.js
+++ src/main/webapp/app/shared/configs/TranslateConfig.js
@@ -1,19 +1,63 @@
 TatamiApp.config(function($translateProvider) {
   
     $translateProvider.translations('en', {
-        'tatami' : {
-            'timeline' : 'Timeline',
-            'mentions' : 'Mentions',
-            'favorites' : 'Favorites',
+        'tatami': {
+            'timeline': 'Timeline',
+            
+            'home': {    
+                'mentions': 'Mentions',
+                'favorites': 'Favorites'
+            },
 
-            'status' : {
-                'replyto' : 'In reply to',
-                'private' : 'Private Message',
-                'view' : 'View',
-                'reply' : 'Reply',
-                'delete' : 'Delete',
-                'share' : 'Share',
-                'favorite' : 'Favorite'
+            'status': {
+                'replyto': 'In reply to',
+                'private': 'Private Message',
+                'view': 'View',
+                'reply': 'Reply',
+                'delete': 'Delete',
+                'share': 'Share',
+                'favorite': 'Favorite'
+            },
+
+            'menu': {
+                'logo': 'Ippon Technologies Logo',
+                'title': 'Tatami',
+                'about': {
+                    'about': 'About',
+                    'presentation': 'Presentation',
+                    'tos': 'Terms of Service',
+                    'language': {
+                        'language': 'Language',
+                        'en': 'English',
+                        'fr': 'Français' // Change to French?
+                    },
+                    'license': 'Software License',
+                    'github': {
+                        'issues': 'Submit a Bug Report',
+                        'fork': 'Fork Tatami on Github'
+                    },
+                    'ippon': {
+                        'website': 'Ippon Technologies Website',
+                        'blog': 'Ippon Technologies Blog',
+                        'twitter': 'Follow @ippontech on Twitter'
+                    }
+                },
+                'search': 'Search',
+                'help': 'Help',
+                'account': {
+                    'account': 'Account',
+                    'profile': 'Profile',
+                    'preferences': 'Preferences',
+                    'password': 'Password',
+                    'files': 'Files',
+                    'users': 'Users',
+                    'groups': 'Groups',
+                    'tags': 'Tags',
+                    'sotd': 'Statuses of the Day',
+                    'companywall': 'Company Wall',
+                    'logout': 'Logout'
+                },
+                'post': 'Post'
             }
         }
     });
