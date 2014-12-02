6290c82f6fe9eeeab0d858880aed73cf507a56b1
jnarron <jnarron@users.noreply.github.com>
2014-12-02 01:12:56 -0500

    start refactoring of profile sidebar

--- src/main/webapp/app/components/profile/sidebar/UserController.js
+++
@@ -1,5 +0,0 @@
-ProfileSideModule.controller('UserController', ['$scope', 'ProfileService', function($scope, ProfileService) {
-    $scope.user = ProfileService.get(function() {
-        $scope.avatarURL = $scope.user.avatar=='' ? '/img/default_image_profile.png' : '/tatami/avatar/' + $scope.user.avatar + '/photo.jpg';
-    })
-}]);
