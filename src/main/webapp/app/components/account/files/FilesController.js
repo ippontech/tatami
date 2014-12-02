f29d69d72ddb074cb9f0a33ccb49276743be851f
hegek87 <hegek87@users.noreply.github.com>
2014-12-01 23:10:10 -0500

    Working on Files account section

--- src/main/webapp/app/components/account/files/FilesController.js
+++ src/main/webapp/app/components/account/files/FilesController.js
@@ -1,3 +1,49 @@
-FilesModule.controller('FilesController', function($scope) {
+FilesModule.controller('FilesController', ['$scope', 'FilesService', function($scope, FilesService) {
 
-});
+    /**
+     * Gets the total % of the files currently uploaded to the server.
+     */
+    $scope.getQuota = function() {
+        FilesService.getQuota(function(result) {
+            $scope.quota = result[0];
+        });
+    };
+
+    /**
+     * Get the array of files from the server using FilesService
+     */
+    $scope.getFiles = function (){
+        FilesService.query(function(result) {
+            $scope.fileList = result;
+        })
+    };
+
+    // Initialize stuff
+    $scope.getQuota();
+    $scope.getFiles();
+
+    /**
+     * Allows us to delete the supplied attachment
+     * @param attachment
+     */
+    $scope.delete = function(attachment) {
+        FilesService.delete({attachmentId: attachment}, { },
+            function() {
+                $scope.remove(attachment);
+                $scope.getQuota();
+            });
+    };
+
+    $scope.remove = function (file, removalIndex){
+        $scope.fileList.splice(removalIndex, 1);
+    };
+
+    $scope.getImgPath = function(thumbnail, attachmentId, filename) {
+        if(thumbnail) {
+            return '/tatami/thumbnail/' + attachmentId + '/' + filename;
+        }
+        else {
+            return '/img/document_icon.png';
+        }
+    };
+}]);
--- src/main/webapp/app/components/account/files/FilesService.js
+++ src/main/webapp/app/components/account/files/FilesService.js
@@ -1,3 +1,8 @@
 FilesModule.factory('FilesService', function($resource) {
-
+    return $resource(
+        '/tatami/rest/attachments/:attachmentId',
+        { },
+        {
+            'getQuota': { method: 'GET', isArray: true, url: '/tatami/rest/attachments/quota' }
+        });
 });
