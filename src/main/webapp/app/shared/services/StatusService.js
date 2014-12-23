d3f42f4af953141b506476d7e65ad23874fc774e
jnarron <jnarron@users.noreply.github.com>
2014-12-23 18:33:46 -0500

    updated reply display in reply modal

--- src/main/webapp/app/shared/services/StatusService.js
+++ src/main/webapp/app/shared/services/StatusService.js
@@ -1,7 +1,13 @@
 TatamiApp.factory('StatusService', ['$resource', function($resource) {
     return $resource(
         '/tatami/rest/statuses/:statusId', null,
-        {   
+        {   'get': { 
+            method: 'GET',
+            transformResponse: function(status, headersGetter) {
+                status = angular.fromJson(status);
+                status.avatarURL = status.avatar=='' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + status.avatar + '/photo.jpg';
+                return status;
+             } },
             'update': { method: 'PATCH', params: { statusId: '@statusId' } }
         });
 }]);
