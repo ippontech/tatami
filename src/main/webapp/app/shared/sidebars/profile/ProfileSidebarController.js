5569c7ca1889d6481119712eac6a66e1e6d204ca
jnarron <jnarron@users.noreply.github.com>
2014-12-24 08:36:14 -0500

    added $stateParams to $rootScope

--- src/main/webapp/app/shared/sidebars/profile/ProfileSidebarController.js
+++ src/main/webapp/app/shared/sidebars/profile/ProfileSidebarController.js
@@ -1,8 +1,8 @@
-ProfileSidebarModule.controller('ProfileSidebarController', ['$scope', '$stateParams', 'UserService', 'TagService', 
-    function($scope, $stateParams, UserService, TagService) {
-        $scope.profile = UserService.get({ username: $stateParams.username });
+ProfileSidebarModule.controller('ProfileSidebarController', ['$scope', '$rootScope', 'UserService', 'TagService',
+    function($scope, $rootScope, UserService, TagService) {
+        $scope.profile = UserService.get({ username: $rootScope.$stateParams.username });
 
-        $scope.tags = TagService.query({ popular: true, user: $stateParams.username });
+        $scope.tags = TagService.query({ popular: true, user: $rootScope.$stateParams.username });
 
         $scope.followTag = function(tag, index) {
             TagService.follow({ tag: tag.name }, { name: tag.name, followed: !tag.followed, trendingUp: tag.trendingUp }, 
