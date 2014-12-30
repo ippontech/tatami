96f7d9587da72d222185b1c57485acd334106aec
jnarron <jnarron@users.noreply.github.com>
2014-12-30 07:55:25 -0500

    got rid of unnecessary getAvatar function
    added page title header to users and tags views

--- src/main/webapp/app/components/account/users/UsersController.js
+++ src/main/webapp/app/components/account/users/UsersController.js
@@ -1,4 +1,4 @@
-UsersModule.controller('UsersController', ['$scope', 'usersList', 'SearchService', 'UserService', function($scope, usersList, SearchService, UserService){
+UsersModule.controller('UsersController', ['$scope', 'usersList', 'SearchService', 'UserService', function($scope, usersList, SearchService, UserService) {
     /**
      * Information about the current state of the view
      * @type {{searchString: string}}
@@ -14,32 +14,23 @@ UsersModule.controller('UsersController', ['$scope', 'usersList', 'SearchService
      * Change the state (so the url contains the search parameter), and updates
      * the user data based on the search term.
      */
-    $scope.search = function (){
+    $scope.search = function() {
         // Update the route
         $scope.$state.transitionTo('account.users.search',
             { q: $scope.current.searchString },
             { location: true, inherit: true, relative: $scope.$state.$current, notify: false });
 
         // Now update the users based on the search term
-        SearchService.query({term: 'users', q: $scope.current.searchString }, function (result){
+        SearchService.query({ term: 'users', q: $scope.current.searchString }, function(result) {
             $scope.usersList = result;
         });
     };
 
-    $scope.followUser = function (user, index){
+    $scope.followUser = function(user, index) {
         UserService.follow({ username: user.username }, { friend: !user.friend, friendShip: true },
-            function (response){
+            function(response) {
                 $scope.$state.reload();
             }
         );
     };
-
-    $scope.getAvatar = function(user) {
-        if(user.avatar){
-            return "/tatami/avatar/" + user.avatar + "/photo.jpg"
-        }
-        else {
-            return "/assets/img/default_image_profile.png"
-        }
-    }
 }]);
--- src/main/webapp/app/shared/services/SearchService.js
+++ src/main/webapp/app/shared/services/SearchService.js
@@ -1,3 +1,18 @@
-TatamiApp.factory('SearchService', ['$resource', function($resource){
-    return $resource('/tatami/rest/search/:term');
+TatamiApp.factory('SearchService', ['$resource', function($resource) {
+    var responseTransform = function(users, headersGetter) {
+        users = angular.fromJson(users);
+
+        for(var i = 0; i < users.length; i++) {
+            users[i]['avatarURL'] = users[i].avatar=='' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + users[i].avatar + '/photo.jpg';
+        }
+
+        return users;
+    };
+
+    return $resource('/tatami/rest/search/:term', { term: '@term' },
+    {
+        'query': { 
+            method: 'GET', isArray: true, transformResponse: responseTransform
+        }
+    });
 }]);
