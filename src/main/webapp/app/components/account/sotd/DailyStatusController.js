c739a808184a91ff3c7bb2a9c84fed505543f726
hegek87 <hegek87@users.noreply.github.com>
2015-01-19 23:59:35 -0500

    Mime type on daily stats fixed. Nearly finished with daily stats

--- src/main/webapp/app/components/account/sotd/DailyStatusController.js
+++ src/main/webapp/app/components/account/sotd/DailyStatusController.js
@@ -1,22 +1,7 @@
-DailyStatusModule.controller('DailyStatusController', ['$scope', 'dailyStats', 'UserService', function($scope, dailyStats, UserService) {
-    $scope.popularUsers = [];
-    dailyStats.$promise.then(function(result) {
-        var data = [];
-        for(var i = 0; i < result.length; ++i) {
-            data.push(UserService.get({ username: result[i].username }));
-            data[i].dailyCount = result[i].statusCount;
-        }
-        return data;
-    }).then(function(popularUsers) {
-        popularUsers.sort(function(a, b) {
-            return a.dailyCount < b.dailyCount ? 1 : -1;
-        });
-        /* Not ideal. Even though popularUsers[i].dailyCount exists, inserting
-        * {{ user.dailyCount }} in the html does not display the count. The work around
-        * is to create a 2 dimensional array
-         */
-        for(var i = 0; i < popularUsers.length; ++i) {
-            $scope.popularUsers.push([popularUsers[i], popularUsers[i].dailyCount]);
-        }
+DailyStatusModule.controller('DailyStatusController', ['$scope', 'dailyStats', 'UserService', 'userData', function($scope, dailyStats, UserService, userData) {
+    $scope.popularUsers = userData;
+
+    $scope.popularUsers.sort(function(a, b) {
+        return a.dailyCount < b.dailyCount ? 1 : -1;
     });
 }]);
