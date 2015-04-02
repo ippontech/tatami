0327662fb653f8d7ef0761b77db755294bed1861
Ben Scott <bscott@ipponusa.com>
2015-04-02 15:16:46 -0400

    fixed start script

--- jenkinsScripts/startTatami.sh
+++ jenkinsScripts/startTatami.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #Insert google auth variables into the build. 
-googleKey=$(cat ../../googleKey.txt)
-googleSecret=$(cat ../../googleSecret.txt)
-newServer="$(cat ../../ServerName.txt)"
+googleKey=$(cat ../../../googleKey.txt)
+googleSecret=$(cat ../../../googleSecret.txt)
+newServer="$(cat ../../../ServerName.txt)"
 
 replaceKey='${tatami.google.clientId}'
 replaceSecret='${tatami.google.clientSecret}'
 oldServer="<tatami.url>http:\/\/localhost:8080<\/tatami.url>"
 
-find src -name *security.xml | xargs sed -i "s/$replaceSecret/$googleSecret/g" 
-find src -name *security.xml | xargs sed -i "s/$replaceKey/$googleKey/g" 
-sed -i "s/$oldServer/$newServer/g" pom.xml
+find ../src -name *security.xml | xargs sed -i "s/$replaceSecret/$googleSecret/g" 
+find ../src -name *security.xml | xargs sed -i "s/$replaceKey/$googleKey/g" 
+sed -i "s/$oldServer/$newServer/g" ../pom.xml
 
 #Start cassandra
 mvn -f ../pom.xml cassandra:run 2> cassandra.error >cassandra.log &
