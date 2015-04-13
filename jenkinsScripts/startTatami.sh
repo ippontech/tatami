04d9424f6e2e3acb4d95dedf70354613787e3e98
Ben Scott <bscott@ipponusa.com>
2015-04-13 11:06:31 -0400

    Seperated inserting google auth/server url from startTatami script

--- jenkinsScripts/startTatami.sh
+++ jenkinsScripts/startTatami.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-#Insert google auth variables into the build. 
-googleKey=$1
-googleSecret=$2
-newServer=$3
+#Insert google Authentication keys.
+./insertGoogleAuthKeys.sh $1 $2 $3
 
-
-usage='startTatami <googleKey> <googleSecret> <serverURL>'
-
-if [ "$#" -ne 3 ]; then
-	echo "Need 3 paremeters"
-        echo "$usage"
+if [ $? -ne 0 ]; then
+    exit 1
 fi
-
-
-replaceKey='${tatami.google.clientId}'
-replaceSecret='${tatami.google.clientSecret}'
-oldServer="<tatami.url>http:\/\/localhost:8080<\/tatami.url>"
-
-find ../src -name *security.xml | xargs sed -i "s/$replaceSecret/$googleSecret/g" 
-find ../src -name *security.xml | xargs sed -i "s/$replaceKey/$googleKey/g" 
-sed -i "s/$oldServer/$newServer/g" ../pom.xml
 
 #Start cassandra
 mvn -f ../pom.xml cassandra:run 2> cassandra.error >cassandra.log &
