0f9aac2ee3888a9ec864e9eeb4ad1862eea503ff
Kenny <hegelandka@vcu.edu>
2015-04-14 14:20:44 -0400

    Fixed google auth login issue where user hit back after logging in with an email, then used the google login button

--- src/main/webapp/app/components/login/google/GoogleLoginController.js
+++ src/main/webapp/app/components/login/google/GoogleLoginController.js
@@ -1,27 +1,9 @@
-LoginModule.controller('GoogleLoginController', ['$scope', '$http', function($scope, $http) {
-    $scope.login = function() {
-        $http({
-            method: 'POST',
-            url: '/tatami/j_spring_openid_security_check',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Access-Control-Allow-Access': '*'
-            },
-            transformRequest: function(obj) {
-                var str = [];
-                for(var p in obj)
-                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                return str.join("&");
-            },
-            data: { openid_identifier: 'https://www.google.com/accounts/o8/id' }
-        })
-            .success(function(data) {
-                console.log('success');
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('error');
-                console.log(data);
-            })
+LoginModule.controller('GoogleLoginController', ['$scope', '$http', 'UserSession', function($scope, $http, UserSession) {
+    $scope.logout = function() {
+        $http.get('/tatami/logout')
+            .success(function() {
+                UserSession.clearSession();
+                $scope.$state.go('tatami.login.main');
+            });
     }
 }]);
