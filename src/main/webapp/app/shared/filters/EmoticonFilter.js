4fbca78f897aa86afb6511892cce8d47584a4f77
Justin Risch <Justinrisch@gmail.com>
2015-04-27 11:36:29 -0400

    Got rid of unnecessary whitespace in emoticonfilter

--- src/main/webapp/app/shared/filters/EmoticonFilter.js
+++ src/main/webapp/app/shared/filters/EmoticonFilter.js
@@ -6,61 +6,37 @@ TatamiApp.filter('emoticon', function() {
 
         var emoticons = {
             '>:(': '/assets/img/emoticons/angry.png',
-
             ':$': '/assets/img/emoticons/blushing.png',
-
             '8)': '/assets/img/emoticons/cool.png',
             'B)': '/assets/img/emoticons/cool.png',
-
             ":'(": '/assets/img/emoticons/crying.png',
-
             ':(': '/assets/img/emoticons/frowning.png',
-
             ':o': '/assets/img/emoticons/gasping.png',
             ':O': '/assets/img/emoticons/gasping.png',
-
             ':D': '/assets/img/emoticons/grinning.png',
-
             '<3': '/assets/img/emoticons/heart.png',
-
             'XD': '/assets/img/emoticons/laughing.png',
-
             ':x': '/assets/img/emoticons/lips_sealed.png',
             ':X': '/assets/img/emoticons/lips_sealed.png',
             ':#': '/assets/img/emoticons/lips_sealed.png',
-
             '>:D': '/assets/img/emoticons/malicious.png',
-
             ':3': '/assets/img/emoticons/naww.png',
-
             ':(': '/assets/img/emoticons/pouting.png',
-
             ':)': '/assets/img/emoticons/smiling.png',
-
             ':|': '/assets/img/emoticons/speechless.png',
-
             '>:)': '/assets/img/emoticons/spiteful.png',
-
             'o_O': '/assets/img/emoticons/surprised.png',
-
             'D:': '/assets/img/emoticons/terrified.png',
-
             ':-1:': '/assets/img/emoticons/thumbs_down.png',
-
             ':+1:': '/assets/img/emoticons/thumbs_up.png',
-
             'XP': '/assets/img/emoticons/tongue_out_laughing.png',
-
             ':p': '/assets/img/emoticons/tongue_out.png',
             ':P': '/assets/img/emoticons/tongue_out.png',
-
             ':/': '/assets/img/emoticons/unsure.png',
-
             ';)': '/assets/img/emoticons/winking_grinning.png',
-
             ';p': '/assets/img/emoticons/winking_tongue_out.png',
             ';P': '/assets/img/emoticons/winking_tongue_out.png',
-
+            ':troll':'/assets/img/emoticons/trollface.png',
             ';)': '/assets/img/emoticons/winking.png'
         };
 
