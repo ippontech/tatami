c561b3add72b604202be8bec02e470e8fe795436
Ben Scott <bscott@ipponusa.com>
2015-04-27 14:55:01 -0400

    basic test

--- src/test/javascript/webapp/app/components/about/license/LicenseController_spec.js
+++ src/test/javascript/webapp/app/components/about/license/LicenseController_spec.js
@@ -5,14 +5,8 @@ describe("License controller test", function() {
     var $stateProvider;
 
 
-    beforeEach(module('AboutModule') {
-    };
+    beforeEach(module('AboutModule')); 
 
-    beforeEach(inject(function($rootScope, $controller, $stateProvider) ) {
-        scope = $rootScope.$new();
-        ctrl = $controller('LicenseController', {$scope, $scope});
-        $stateProvidr = stateMock;
-    }));
 
     it('Sanity check', function() {
         expect(true).toBeTruthy();
