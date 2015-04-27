5a06f68c4c9eb5a29050c1904bacf80767267efc
Justin Risch <Justinrisch@gmail.com>
2015-04-27 11:02:40 -0400

    Moved repeated variable to higher scope

--- src/main/java/fr/ippon/tatami/config/CassandraConfiguration.java
+++ src/main/java/fr/ippon/tatami/config/CassandraConfiguration.java
@@ -40,6 +40,7 @@ public class CassandraConfiguration {
     private Environment env;
 
     private Cluster myCluster;
+    private String cassandraKeyspace = this.env.getProperty("cassandra.keyspace");
 
     @PreDestroy
     public void destroy() {
@@ -53,7 +54,6 @@ public class CassandraConfiguration {
         log.info("Configuring Cassandra keyspace");
         String cassandraHost = env.getProperty("cassandra.host");
         String cassandraClusterName = env.getProperty("cassandra.clusterName");
-        String cassandraKeyspace = env.getProperty("cassandra.keyspace");
 
         CassandraHostConfigurator cassandraHostConfigurator = new CassandraHostConfigurator(cassandraHost);
         cassandraHostConfigurator.setMaxActive(100);
@@ -129,9 +129,7 @@ public class CassandraConfiguration {
 
     private void addColumnFamily(ThriftCluster cluster, String cfName, int rowCacheKeysToSave) {
 
-        String cassandraKeyspace = this.env.getProperty("cassandra.keyspace");
-
-        ColumnFamilyDefinition cfd =
+      ColumnFamilyDefinition cfd =
                 HFactory.createColumnFamilyDefinition(cassandraKeyspace, cfName);
 
         cfd.setRowCacheKeysToSave(rowCacheKeysToSave);
@@ -139,8 +137,6 @@ public class CassandraConfiguration {
     }
 
     private void addColumnFamilySortedbyUUID(ThriftCluster cluster, String cfName, int rowCacheKeysToSave) {
-
-        String cassandraKeyspace = this.env.getProperty("cassandra.keyspace");
 
         ColumnFamilyDefinition cfd =
                 HFactory.createColumnFamilyDefinition(cassandraKeyspace, cfName);
@@ -152,8 +148,6 @@ public class CassandraConfiguration {
 
 
     private void addColumnFamilyCounter(ThriftCluster cluster, String cfName, int rowCacheKeysToSave) {
-        String cassandraKeyspace = this.env.getProperty("cassandra.keyspace");
-
         ThriftCfDef cfd =
                 new ThriftCfDef(cassandraKeyspace, cfName, ComparatorType.UTF8TYPE);
 
