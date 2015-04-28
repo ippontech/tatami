2fbef906d98ab298c067af2d06a2965fa0f1861a
Ben Scott <bscott@ipponusa.com>
2015-04-28 13:34:03 -0400

    Added about module testing

---
+++ src/test/javascript/webapp/app/components/about/AboutModule_spec.js
@@ -0,0 +1,18 @@
+describe("About module routing test", function() {
+
+    var rootScope, state, injector;
+    beforeEach(module("ui.router"));
+    beforeEach(module("AboutModule"));
+
+    beforeEach(inject(function ($rootScope, $state, $injector) {
+        rootScope = $rootScope;
+        state = $state;
+        injector = $injector;
+    }));
+
+    it('sanity check', function() {
+        expect(true).toBeTruthy(); 
+    });
+
+
+});
