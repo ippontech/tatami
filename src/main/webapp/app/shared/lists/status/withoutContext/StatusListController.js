274c82abda1005376b7fbe9a58f3846e6a919c3e
Jeremy Narron <jnarron@users.noreply.github.com>
2015-04-29 00:47:37 -0400

    fixed polling, removed angular-poller, code cleanup

--- src/main/webapp/app/shared/lists/status/withoutContext/StatusListController.js
+++ src/main/webapp/app/shared/lists/status/withoutContext/StatusListController.js
@@ -11,9 +11,8 @@ HomeModule.controller('StatusListController', [
     'statuses',
     'userRoles',
     'showModal',
-    'poller',
-    function($scope, $timeout, $window, $translate, StatusService, HomeService, TagService, GroupService, profile, statuses, userRoles, showModal, poller) {
-        if(showModal && $scope.$state.includes('tatami.home.home.timeline')) {
+    function($scope, $timeout, $window, $translate, StatusService, HomeService, TagService, GroupService, profile, statuses, userRoles, showModal) {
+        if(showModal && $scope.$state.is('tatami.home.home.timeline')) {
             $scope.$state.go('tatami.home.home.timeline.presentation');
         }
 
@@ -30,45 +29,157 @@ HomeModule.controller('StatusListController', [
             $scope.finish = $scope.statuses[$scope.statuses.length - 1].timelineId;
         }
 
+        /* Begin Polling Related Code */
+
         $scope.newMessages = null;
         $window.document.title = 'Tatami';
 
-        if($scope.$state.is('tatami.home.home.timeline')) {
+        var requestNewStatuses = function() {
             var pollingDelay = 20000; // In milliseconds
 
-            $timeout(function() {
-                var argument = {};
+            $scope.poller = $timeout(function() {
+                var arguments = null;
 
-                if($scope.statuses.length != 0) {
-                    argument = { start: statuses[0].timelineId };
+                if($scope.statuses.length > 0) {
+                    if($scope.$state.is('tatami.home.home.tag')) {
+                        arguments = { tag: $scope.$stateParams.tag, start: statuses[0].timelineId };
+                    }
+
+                    else if($scope.$state.is('tatami.home.home.group.statuses')) {
+                        arguments = { groupId: $scope.$stateParams.groupId, start: statuses[0].timelineId };
+                    }
+
+                    else if($scope.$state.is('tatami.home.profile.statuses')) {
+                        arguments = { username: $scope.$stateParams.username, start: statuses[0].timelineId };
+                    }
+
+                    else {
+                        arguments = { start: statuses[0].timelineId };
+                    }
+                }
+                else {
+                    if($scope.$state.is('tatami.home.home.tag')) {
+                        arguments = { tag: $scope.$stateParams.tag };
+                    }
+
+                    else if($scope.$state.is('tatami.home.home.group.statuses')) {
+                        arguments = { groupId: $scope.$stateParams.groupId };
+                    }
+
+                    else if($scope.$state.is('tatami.home.profile.statuses')) {
+                        arguments = { username: $scope.$stateParams.username };
+                    }
+
+                    else {
+                        arguments = {};
+                    }
                 }
 
-                var statusPoller = poller.get(StatusService, {
-                    action: 'getHomeTimeline',
-                    delay: pollingDelay,
-                    smart: true,
-                    argumentsArray: [ argument ]
-                });
-
-                statusPoller.promise.then(null, null, function(response) {
+                var success = function(response) {
                     if(response.length > 0) {
                         $scope.newMessages = response;
-                        $window.document.title = 'Tatami (' + response.length + ')';
+                        $window.document.title = 'Tatami (' + $scope.newMessages.length + ')';
                     }
-                });
+                    requestNewStatuses();
+                };
+
+                var error = function(err) {
+                    requestNewStatuses();
+                };
+
+                if($scope.$state.is('tatami.home.home.timeline')) {
+                    StatusService.getHomeTimeline(arguments, success, error);
+                }
+
+                else if($scope.$state.is('tatami.home.home.mentions')) {
+                    HomeService.getMentions(arguments, success, error);
+                }
+
+                else if($scope.$state.is('tatami.home.home.company')) {
+                    HomeService.getCompanyTimeline(arguments, success, error);
+                }
+
+                else if($scope.$state.is('tatami.home.home.tag')) {
+                    TagService.getTagTimeline(arguments, success, error);
+                }
+
+                else if($scope.$state.is('tatami.home.home.group.statuses')) {
+                    GroupService.getStatuses(arguments, success, error);
+                }
+
+                else if($scope.$state.is('tatami.home.profile.statuses')) {
+                    StatusService.getUserTimeline(arguments, success, error);
+                }
             }, pollingDelay);
-        }
+        };
+
+        requestNewStatuses();
+
+        $scope.$on('$destroy', function() {
+            $timeout.cancel($scope.poller);
+        });
 
         $scope.loadNewStatuses = function() {
             for(var i = $scope.newMessages.length - 1; i >= 0 ; i--) {
                 $scope.statuses.unshift($scope.newMessages[i]);
             }
-            
-            $window.document.title = 'Tatami';
+
             $scope.newMessages = null;
+            $window.document.title = 'Tatami';
         };
 
-        var loadMoreSuccess = function(statuses) {
+        /* End Polling Related Code */
+
+        /* Begin Infinite Scrolling Related Code */
+
+        $scope.requestOldStatuses = function() {
+            if($scope.busy || $scope.end) {
+                return;
+            }
+
+            $scope.busy = true;
+
+            if($scope.$state.is('tatami.home.home.timeline')) {
+                StatusService.getHomeTimeline({ finish: $scope.finish }, loadOldStatuses);
+            }
+
+            else if($scope.$state.is('tatami.home.home.company')) {
+                HomeService.getCompanyTimeline({ finish: $scope.finish }, loadOldStatuses);
+            }
+
+            else if($scope.$state.is('tatami.home.home.mentions')) {
+                HomeService.getMentions({ finish: $scope.finish }, loadOldStatuses);
+            }
+
+            /*
+                Favorites are limited to 50 total per user. All 50 are loaded
+                from the favorites REST endpoint at once. There is no way to use
+                &finish=timelineId for favorites as of now in the backend.
+
+                Keep this commented out until the backend is changed to allow
+                for more than 50 favorites and adding &finish=timelineId to the
+                REST url.
+            */
+            /*
+            else if($scope.$state.is('tatami.home.home.favorites')) {
+                HomeService.getFavorites({ finish: $scope.finish }, loadOldStatuses);
+            }
+            */
+
+            else if($scope.$state.is('tatami.home.home.tag')) {
+                TagService.getTagTimeline({ tag: $scope.$stateParams.tag, finish: $scope.finish }, loadOldStatuses);
+            }
+
+            else if($scope.$state.is('tatami.home.home.group.statuses')) {
+                GroupService.getStatuses({ groupId: $scope.$stateParams.groupId, finish: $scope.finish }, loadOldStatuses);
+            }
+
+            else if($scope.$state.is('tatami.home.profile.statuses')) {
+                StatusService.getUserTimeline({ username: $scope.$stateParams.username, finish: $scope.finish }, loadOldStatuses);
+            }
+        };
+
+        var loadOldStatuses = function(statuses) {
             if(statuses.length == 0) {
                 $scope.end = true; // reached end of list
                 return;
@@ -82,52 +193,7 @@ HomeModule.controller('StatusListController', [
             $scope.busy = false;
         };
 
-        $scope.loadMore = function() {
-            if($scope.busy || $scope.end) {
-                return;
-            }
-
-            $scope.busy = true;
-
-            if($scope.$state.current.name == 'tatami.home.home.timeline') {
-                StatusService.getHomeTimeline({ finish: $scope.finish }, loadMoreSuccess);
-            }
-
-            if($scope.$state.current.name == 'tatami.home.home.company') {
-                HomeService.getCompanyTimeline({ finish: $scope.finish }, loadMoreSuccess);
-            }
-
-            if($scope.$state.current.name == 'tatami.home.home.mentions') {
-                HomeService.getMentions({ finish: $scope.finish }, loadMoreSuccess);
-            }
-
-            /*
-                Favorites are limited to 50 total per user. All 50 are loaded
-                from the favorites REST endpoint at once. There is no way to use
-                &finish=timelineId for favorites as of now in the backend.
-
-                Keep this commented out until the backend is changed to allow
-                for more than 50 favorites and adding &finish=timelineId to the
-                REST url.
-            */
-            /*
-            if($scope.$state.current.name == 'tatami.home.home.favorites') {
-                HomeService.getFavorites({ finish: $scope.finish }, loadMoreSuccess);
-            }
-            */
-
-            if($scope.$state.current.name == 'tatami.home.home.tag') {
-                TagService.getTagTimeline({ tag: $scope.$stateParams.tag, finish: $scope.finish }, loadMoreSuccess);
-            }
-
-            if($scope.$state.current.name == 'tatami.home.home.group.statuses') {
-                GroupService.getStatuses({ groupId: $scope.$stateParams.groupId, finish: $scope.finish }, loadMoreSuccess);
-            }
-
-            if($scope.$state.current.name == 'tatami.home.profile.statuses') {
-                StatusService.getUserTimeline({ username: $scope.$stateParams.username, finish: $scope.finish }, loadMoreSuccess);
-            }
-        };
+        /* End Infinite Scrolling Related Code */
 
         $scope.isOneDayOrMore = function(date) {
             return moment().diff(moment(date), 'days', true) >= 1;
@@ -159,7 +225,7 @@ HomeModule.controller('StatusListController', [
             StatusService.update({ statusId: status.statusId }, { announced: true },
                 function() {
                     $scope.$state.reload();
-                    //$scope.$state.transitionTo('tatami.home.home.timeline');
+                    //$scope.$state.transitionTo('tatami.home.home.timeline', {}, { reload: true });
             });
         };
 
