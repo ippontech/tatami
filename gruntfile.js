5d50fcc13dd8cddfe3f50838d000d811391e4d74
Justin Risch <Justinrisch@gmail.com>
2015-04-29 13:03:46 -0400

    removed accidentally left line

--- gruntfile.js
+++ gruntfile.js
@@ -85,6 +85,5 @@ module.exports = function(grunt) {
 
    } );
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-stripcomments');
     grunt.registerTask('default', ['uglify']);
 };
