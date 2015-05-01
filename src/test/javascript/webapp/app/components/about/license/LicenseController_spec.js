60f1b147b004a6e5d07cbb74902fafd83b83f122
Ben Scott <bscott@ipponusa.com>
2015-05-01 14:02:45 -0400

    Finished LicenseController test

--- src/test/javascript/webapp/app/components/about/license/LicenseController_spec.js
+++ src/test/javascript/webapp/app/components/about/license/LicenseController_spec.js
@@ -1,9 +1,15 @@
 describe("License controller test", function() {
-    beforeEach(module('ui.router'))
+    beforeEach(module('ui.router'));
     beforeEach(module('AboutModule'));
 
-    it('Sanity check', function() {
-        expect(4).toBeTruthy();
+    beforeEach(inject(function(_$controller_) {
+       $controller = _$controller_; 
+    }));
+
+    it('Checks that LicenseController injects current year in the copyright', function() {
+        var $scope = {};
+        var controller = $controller('LicenseController', {$scope: $scope});
+        expect($scope.endYear).toBe( new Date().getFullYear());
     });
 
 });
