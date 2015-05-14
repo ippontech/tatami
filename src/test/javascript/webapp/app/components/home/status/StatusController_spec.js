6f300eca90800d9b068f6dfb52988f70c458cecc
Justin Risch <Justinrisch@gmail.com>
2015-05-14 09:55:35 -0400

    StatusController_spec was returning false positive.
    Lines 49-51 (now 39-41)

--- src/test/javascript/webapp/app/components/home/status/StatusController_spec.js
+++ src/test/javascript/webapp/app/components/home/status/StatusController_spec.js
@@ -1,9 +1,9 @@
-describe("Status Controller Test", function() {
+describe("Status Controller Test", function () {
     var ctrl, statusService;
     beforeEach(module('ui.router'));
     beforeEach(module('HomeModule'));
     beforeEach(module('pascalprecht.translate'));
-    
+
     //Mocked password service:
     beforeEach(module({
         StatusService: {
@@ -12,42 +12,32 @@ describe("Status Controller Test", function() {
         }
     }));
 
-    /*'StatusController', [
-     '$scope',
-     '$translate',
-     'StatusService',
-     'profile',
-     'status',
-     'context',
-     'userRoles',*/
 
-
-    beforeEach(inject(function($rootScope, _$controller_, _$translate_, _StatusService_) {
+    beforeEach(inject(function ($rootScope, _$controller_, _$translate_, _StatusService_) {
         $scope = $rootScope.$new();
         $translate = _$translate_;
 
         statusService = _StatusService_;
         spyOn(statusService, 'save');
         ctrl = _$controller_('StatusController', {
-            $scope : $scope,
-            $translate : $translate,
-            'StatusService' : statusService,
-            'profile':{},
-            'status':'teehee',
-            'context':{discussionStatuses:{}},
-            'userRoles':{roles:['ROLE_ADMIN']}
+            $scope: $scope,
+            $translate: $translate,
+            'StatusService': statusService,
+            'profile': {},
+            'status': 'teehee',
+            'context': {discussionStatuses: []},
+            'userRoles': {roles: ['', '', 'ROLE_ADMIN']}
         });
     }));
 
 
-    it('SANITY', function() {
-       expect(true).toBeTruthy();
+    it('SANITY', function () {
+        expect(true).toBeTruthy();
     });
-    it('Is Admin check', function(){
+    it('Is Admin check', function () {
         expect($scope.isAdmin).toBeTruthy();
     });
-    it('Status accepted without discussion', function(){
-        expect($scope.statuses[0]==$scope.status).toBeTruthy();
+    it('Status accepted without discussion', function () {
+        expect($scope.statuses[0] == 'teehee').toBeTruthy();
     });
-
 });
