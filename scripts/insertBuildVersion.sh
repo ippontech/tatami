1bb75ff8b04748690ac7949edb1427d26c53cc05
kenny <khegeland@ipponusa.com>
2015-07-07 14:46:51 -0400

    Updated script to insert build and version to home sidebar

--- scripts/insertBuildVersion.sh
+++ scripts/insertBuildVersion.sh
@@ -15,8 +15,11 @@ fi
 
 find . -name FooterView.html | xargs $sedi "s/<pom>version<\/pom>/$1/"
 find . -name FooterView.html | xargs $sedi "s/<pom>build<\/pom>/$2/"
+find . -name HomeSidebarView.html | xargs $sedi "s/<pom>version<\/pom>/$1/"
+find . -name HomeSidebarView.html | xargs $sedi "s/<pom>build<\/pom>/$2/"
 
 if $darwin; then
     find . -name TopMenuView.html.sed.del | xargs rm
+    find . -name HomeSidebarView.html.sed.del | xargs rm
 fi
 
