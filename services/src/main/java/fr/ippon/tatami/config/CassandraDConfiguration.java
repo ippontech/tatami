1475ce68bae77df27d7f2bfb6d6f3d512494947e
Lars Nørregaard <lnorregaard@nordija.com>
2015-11-20 16:05:22 +0100

    Added Spring data configuration to replace all CRUD methods

--- services/src/main/java/fr/ippon/tatami/config/CassandraDConfiguration.java
+++ services/src/main/java/fr/ippon/tatami/config/CassandraDConfiguration.java
@@ -1,19 +1,25 @@
 package fr.ippon.tatami.config;
 
-//import com.netflix.astyanax.AstyanaxContext;
-//import com.netflix.astyanax.Cluster;
-//import com.netflix.astyanax.Keyspace;
-//import com.netflix.astyanax.connectionpool.NodeDiscoveryType;
-//import com.netflix.astyanax.connectionpool.impl.ConnectionPoolConfigurationImpl;
-//import com.netflix.astyanax.connectionpool.impl.CountingConnectionPoolMonitor;
-//import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
-//import com.netflix.astyanax.thrift.ThriftFamilyFactory;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
 import me.prettyprint.hector.api.Keyspace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.config.CassandraSessionFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.convert.CassandraConverter;
+import org.springframework.data.cassandra.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext;
+import org.springframework.data.cassandra.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
@@ -21,154 +27,52 @@ import javax.inject.Inject;
 /**
  * Cassandra configuration file.
  *
- * @author Julien Dubois
+ * @author Lars Nørregaard
  */
+@Configuration
+@PropertySource(value = { "classpath:cassandra.properties" })
+@EnableCassandraRepositories(basePackages = { "fr.ippon.tatami.repository" })
 public class CassandraDConfiguration {
 
-    private final Logger log = LoggerFactory.getLogger(CassandraDConfiguration.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CassandraDConfiguration.class);
 
-    @Inject
+    @Autowired
     private Environment env;
 
-//    private Cluster myCluster;
+    @Bean
+    public CassandraClusterFactoryBean cluster() {
 
-    @PreDestroy
-    public void destroy() {
-        log.info("Closing Hector connection pool");
-//        HFactory.shutdownCluster(myCluster);
+        CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
+        cluster.setContactPoints(env.getProperty("cassandra.host"));
+        cluster.setPort(Integer.parseInt(env.getProperty("cassandra.port")));
+
+        return cluster;
     }
 
-//    @Bean
-    public Keyspace keyspaceOperator() {
-        log.info("Configuring Cassandra keyspace");
-//        String cassandraHost = env.getProperty("cassandra.host");
-//        String cassandraClusterName = env.getProperty("cassandra.clusterName");
-//        String cassandraKeyspace = env.getProperty("cassandra.keyspace");
-//        AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
-//                .forCluster(cassandraClusterName)
-//                .forKeyspace(cassandraKeyspace)
-//                .withAstyanaxConfiguration(new AstyanaxConfigurationImpl()
-//                        .setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE)
-//                        .setCqlVersion("3.3.0")
-//                        .setTargetCassandraVersion("2.2")
-//                )
-//                .withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl("MyConnectionPool")
-//                        .setPort(9142)
-//                        .setMaxConnsPerHost(1)
-//                        .setSeeds(cassandraHost)
-//                )
-//                .withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
-//                .buildKeyspace(ThriftFamilyFactory.getInstance());
-//        context.start();
-//        Keyspace keyspace = context.getClient();
-
-        return null;
+    @Bean
+    public CassandraMappingContext mappingContext() {
+        return new BasicCassandraMappingContext();
     }
-//
-//        CassandraHostConfigurator cassandraHostConfigurator = new CassandraHostConfigurator(cassandraHost);
-//        cassandraHostConfigurator.setMaxActive(100);
-//        if (env.acceptsProfiles(Constants.SPRING_PROFILE_METRICS)) {
-//            log.debug("Cassandra Metrics monitoring enabled");
-//            HOpTimer hOpTimer = new MetricsOpTimer(cassandraClusterName);
-//            cassandraHostConfigurator.setOpTimer(hOpTimer);
-//        }
-//        ThriftCluster cluster = new ThriftCluster(cassandraClusterName, cassandraHostConfigurator);
-//        this.myCluster = cluster; // Keep a pointer to the cluster, as Hector is buggy and can't find it again...
-//        ConfigurableConsistencyLevel consistencyLevelPolicy = new ConfigurableConsistencyLevel();
-//        consistencyLevelPolicy.setDefaultReadConsistencyLevel(HConsistencyLevel.ONE);
-//
-//        KeyspaceDefinition keyspaceDef = cluster.describeKeyspace(cassandraKeyspace);
-//        if (keyspaceDef == null) {
-//            log.warn("Keyspace \" {} \" does not exist, creating it!", cassandraKeyspace);
-//            keyspaceDef = new ThriftKsDef(cassandraKeyspace);
-//            cluster.addKeyspace(keyspaceDef, true);
-//
-//            addColumnFamily(cluster, ColumnFamilyKeys.USER_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.FRIENDS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.FOLLOWERS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.STATUS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.DOMAIN_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.REGISTRATION_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.RSS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.MAILDIGEST_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.SHARES_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.DISCUSSION_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.USER_TAGS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.TAG_FOLLOWERS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.GROUP_MEMBERS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.USER_GROUPS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.GROUP_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.GROUP_DETAILS_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.ATTACHMENT_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.AVATAR_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.DOMAIN_CONFIGURATION_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.TATAMIBOT_CONFIGURATION_CF, 0);
-//            addColumnFamily(cluster, ColumnFamilyKeys.APPLE_DEVICE_CF, 0);
-//
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.TIMELINE_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.TIMELINE_SHARES_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.MENTIONLINE_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.USERLINE_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.USERLINE_SHARES_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.FAVLINE_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.TAGLINE_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.TRENDS_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.USER_TRENDS_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.GROUPLINE_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.USER_ATTACHMENT_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.STATUS_ATTACHMENT_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.DOMAINLINE_CF, 0);
-//            addColumnFamilySortedbyUUID(cluster, ColumnFamilyKeys.DOMAIN_TATAMIBOT_CF, 0);
-//
-//            addColumnFamilyCounter(cluster, ColumnFamilyKeys.COUNTER_CF, 0);
-//            addColumnFamilyCounter(cluster, ColumnFamilyKeys.TAG_COUNTER_CF, 0);
-//            addColumnFamilyCounter(cluster, ColumnFamilyKeys.GROUP_COUNTER_CF, 0);
-//            addColumnFamilyCounter(cluster, ColumnFamilyKeys.DAYLINE_CF, 0);
-//
-//            //Tatami Bot CF
-//            addColumnFamily(cluster, ColumnFamilyKeys.TATAMIBOT_DUPLICATE_CF, 0);
-//        }
-//        return HFactory.createKeyspace(cassandraKeyspace, cluster, consistencyLevelPolicy);
-//    }
-//
-//    @Bean
-//    public EntityManagerImpl entityManager(Keyspace keyspace) {
-//        String[] packagesToScan = {"fr.ippon.tatami.domain", "fr.ippon.tatami.bot.config"};
-//        return new EntityManagerImpl(keyspace, packagesToScan);
-//    }
-//
-//    private void addColumnFamily(ThriftCluster cluster, String cfName, int rowCacheKeysToSave) {
-//
-//        String cassandraKeyspace = this.env.getProperty("cassandra.keyspace");
-//
-//        ColumnFamilyDefinition cfd =
-//                HFactory.createColumnFamilyDefinition(cassandraKeyspace, cfName);
-//
-//        cfd.setRowCacheKeysToSave(rowCacheKeysToSave);
-//        cluster.addColumnFamily(cfd);
-//    }
-//
-//    private void addColumnFamilySortedbyUUID(ThriftCluster cluster, String cfName, int rowCacheKeysToSave) {
-//
-//        String cassandraKeyspace = this.env.getProperty("cassandra.keyspace");
-//
-//        ColumnFamilyDefinition cfd =
-//                HFactory.createColumnFamilyDefinition(cassandraKeyspace, cfName);
-//
-//        cfd.setRowCacheKeysToSave(rowCacheKeysToSave);
-//        cfd.setComparatorType(ComparatorType.UUIDTYPE);
-//        cluster.addColumnFamily(cfd);
-//    }
-//
-//
-//    private void addColumnFamilyCounter(ThriftCluster cluster, String cfName, int rowCacheKeysToSave) {
-//        String cassandraKeyspace = this.env.getProperty("cassandra.keyspace");
-//
-//        ThriftCfDef cfd =
-//                new ThriftCfDef(cassandraKeyspace, cfName, ComparatorType.UTF8TYPE);
-//
-//        cfd.setRowCacheKeysToSave(rowCacheKeysToSave);
-//        cfd.setDefaultValidationClass(ComparatorType.COUNTERTYPE.getClassName());
-//        cluster.addColumnFamily(cfd);
-//    }
+
+    @Bean
+    public CassandraConverter converter() {
+        return new MappingCassandraConverter(mappingContext());
+    }
+
+    @Bean
+    public CassandraSessionFactoryBean session() throws Exception {
+
+        CassandraSessionFactoryBean session = new CassandraSessionFactoryBean();
+        session.setCluster(cluster().getObject());
+        session.setKeyspaceName(env.getProperty("cassandra.keyspace"));
+        session.setConverter(converter());
+        session.setSchemaAction(SchemaAction.NONE);
+
+        return session;
+    }
+
+    @Bean
+    public CassandraOperations cassandraTemplate() throws Exception {
+        return new CassandraTemplate(session().getObject());
+    }
 }
