b7c93e7513fb71a140b5f54df85431f3e507af02
Lars Nørregaard <lnorregaard@nordija.com>
2015-11-30 13:31:31 +0100

    Replaced Cassandra code for StatusAttachments

--- services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraStatusAttachmentRepository.java
+++ services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraStatusAttachmentRepository.java
@@ -1,5 +1,9 @@
 package fr.ippon.tatami.repository.cassandra;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 import fr.ippon.tatami.config.Constants;
 import fr.ippon.tatami.repository.StatusAttachmentRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +14,10 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+import static fr.ippon.tatami.config.ColumnFamilyKeys.SHARES_CF;
 import static fr.ippon.tatami.config.ColumnFamilyKeys.STATUS_ATTACHMENT_CF;
 
 /**
@@ -28,39 +35,38 @@ public class CassandraStatusAttachmentRepository
         implements StatusAttachmentRepository {
 
 
-//    @Inject
-
-    @PostConstruct
-    public void init() {
-//        attachmentsTemplate = new ThriftColumnFamilyTemplate<String, UUID>(keyspaceOperator,
-//                STATUS_ATTACHMENT_CF,
-//                StringSerializer.get(),
-//                UUIDSerializer.get());
-//
-//        attachmentsTemplate.setCount(Constants.CASSANDRA_MAX_COLUMNS);
-    }
+    @Inject
+    private Session session;
 
     @Override
     public void addAttachmentId(String statusId, String attachmentId) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.insert(statusId, STATUS_ATTACHMENT_CF, HFactory.createColumn(UUID.fromString(attachmentId),
-//                Calendar.getInstance().getTimeInMillis(), UUIDSerializer.get(), LongSerializer.get()));
+        Statement statement = QueryBuilder.insertInto(STATUS_ATTACHMENT_CF)
+                .value("statusId", UUID.fromString(statusId))
+                .value("attachmentId",UUID.fromString(attachmentId))
+                .value("created",System.currentTimeMillis());
+        session.execute(statement);
     }
 
     @Override
     public void removeAttachmentId(String statusId, String attachmentId) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.delete(statusId, STATUS_ATTACHMENT_CF, UUID.fromString(attachmentId), UUIDSerializer.get());
+        Statement statement = QueryBuilder.delete().from(STATUS_ATTACHMENT_CF)
+                .where(eq("statusId", UUID.fromString(statusId)))
+                .and(eq("attachmentId", UUID.fromString(attachmentId)));
+        session.execute(statement);
     }
 
     @Override
     public Collection<String> findAttachmentIds(String statusId) {
-////        ColumnFamilyResult<String, UUID> result = attachmentsTemplate.queryColumns(statusId);
-////        Collection<String> attachmentIds = new ArrayList<String>();
-////        for (UUID columnName : result.getColumnNames()) {
-////            attachmentIds.add(columnName.toString());
-////        }
-//        return attachmentIds;
-        return null;
+        Statement statement = QueryBuilder.select()
+                .column("attachmentId")
+                .from(STATUS_ATTACHMENT_CF)
+                .where(eq("statusId", statusId))
+                .limit(Constants.CASSANDRA_MAX_ROWS);
+        ResultSet results = session.execute(statement);
+        return results
+                .all()
+                .stream()
+                .map(e -> e.getString("attachmentId"))
+                .collect(Collectors.toList());
     }
 }
