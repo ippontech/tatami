f535bbad7f22731ae57d1a2a11a5d383772bd152
Lars Nørregaard <lnorregaard@nordija.com>
2015-11-30 14:04:49 +0100

    Replaced Cassandra code for UserAttachment

--- services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraUserAttachmentRepository.java
+++ services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraUserAttachmentRepository.java
@@ -1,12 +1,23 @@
 package fr.ippon.tatami.repository.cassandra;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Select;
+import com.datastax.driver.core.utils.UUIDs;
 import fr.ippon.tatami.config.Constants;
+import fr.ippon.tatami.config.GroupRoles;
 import fr.ippon.tatami.repository.UserAttachmentRepository;
 import org.springframework.stereotype.Repository;
 import fr.ippon.tatami.config.ColumnFamilyKeys;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.*;
 
 
 /**
@@ -23,75 +34,59 @@ import java.util.*;
 public class CassandraUserAttachmentRepository
         implements UserAttachmentRepository {
 
-//    private ColumnFamilyTemplate<String, UUID> attachmentsTemplate;
-
-
-    @PostConstruct
-    public void init() {
-//        attachmentsTemplate = new ThriftColumnFamilyTemplate<String, UUID>(keyspaceOperator,
-//                ColumnFamilyKeys.USER_ATTACHMENT_CF,
-//                StringSerializer.get(),
-//                UUIDSerializer.get());
-//
-//        attachmentsTemplate.setCount(Constants.CASSANDRA_MAX_COLUMNS);
-    }
+    @Inject
+    private Session session;
 
     @Override
     public void addAttachmentId(String login, String attachmentId) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.insert(login, ColumnFamilyKeys.USER_ATTACHMENT_CF, HFactory.createColumn(UUID.fromString(attachmentId),
-//                Calendar.getInstance().getTimeInMillis(), UUIDSerializer.get(), LongSerializer.get()));
+        Statement statement = QueryBuilder.insertInto(ColumnFamilyKeys.USER_ATTACHMENT_CF)
+                .value("login", login)
+                .value("attachmentId", UUID.fromString(attachmentId));
+        session.execute(statement);
     }
 
     @Override
     public void removeAttachmentId(String login, String attachmentId) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.delete(login, ColumnFamilyKeys.USER_ATTACHMENT_CF, UUID.fromString(attachmentId), UUIDSerializer.get());
+        Statement statement = QueryBuilder.delete().from(ColumnFamilyKeys.USER_ATTACHMENT_CF)
+                .where(eq("login", login))
+                .and(eq("attachmentId",UUID.fromString(attachmentId)));
+        session.execute(statement);
     }
 
     @Override
     public Collection<String> findAttachmentIds(String login, int pagination, String finish) {
-//        List<HColumn<UUID, Long>> result;
-//        if (finish != null) {
-//        ColumnSlice<UUID, Long> query = createSliceQuery(keyspaceOperator,
-//                StringSerializer.get(), UUIDSerializer.get(), LongSerializer.get())
-//                .setColumnFamily(ColumnFamilyKeys.USER_ATTACHMENT_CF)
-//                .setKey(login)
-//                .setRange(UUID.fromString(finish), null, true, pagination)
-//                .execute()
-//                .get();
-//
-//        result = query.getColumns();
-//        }  else {
-//            ColumnSlice<UUID, Long> query = createSliceQuery(keyspaceOperator,
-//                    StringSerializer.get(), UUIDSerializer.get(), LongSerializer.get())
-//                    .setColumnFamily(ColumnFamilyKeys.USER_ATTACHMENT_CF)
-//                    .setKey(login)
-//                    .setRange(null, null, true, pagination)
-//                    .execute()
-//                    .get();
-//
-//            result = query.getColumns();
-//        }
-//
-//        Collection<String> attachmentIds = new ArrayList<String>();
-//        int index = 0;
-//        for (HColumn<UUID, Long> column : result) {
-//            attachmentIds.add(column.getName().toString());
-//            index++;
-//        }
-//        return attachmentIds;
-        return null;
+        Select.Where where = QueryBuilder.select()
+                .column("attachmentId")
+                .from(ColumnFamilyKeys.USER_ATTACHMENT_CF)
+                .where(eq("login", login));
+        if(finish != null) {
+            where.and(gt("attachmentId", UUID.fromString(finish)));
+        }
+        where.orderBy(desc("attachmentId")).limit(pagination);
+
+        Statement statement = where;
+        ResultSet results = session.execute(statement);
+        return results
+                .all()
+                .stream()
+                .map(e -> e.getUUID("attachmentId").toString())
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<String> findAttachmentIds(String login) {
-//        ColumnFamilyResult<String, UUID> result = attachmentsTemplate.queryColumns(login);
-//        Collection<String> attachmentIds = new ArrayList<String>();
-//        for (UUID columnName : result.getColumnNames()) {
-//            attachmentIds.add(columnName.toString());
-//        }
-//        return attachmentIds;
-        return null;
+        Statement statement = QueryBuilder.select()
+                .column("attachmentId")
+                .from(ColumnFamilyKeys.USER_ATTACHMENT_CF)
+                .where(eq("login", login))
+                .limit(Constants.CASSANDRA_MAX_ROWS);
+
+        ResultSet results = session.execute(statement);
+        return results
+                .all()
+                .stream()
+                .map(e -> e.getString("attachmentId").toString())
+                .collect(Collectors.toList());
+
     }
 }
