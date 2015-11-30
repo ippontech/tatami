f481ff08fc8308a6db72095c5b5a3d957525c0c9
Lars Nørregaard <lnorregaard@nordija.com>
2015-11-30 11:05:22 +0100

    Replaced Cassandra code for GroupCounter

--- services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraGroupCounterRepository.java
+++ services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraGroupCounterRepository.java
@@ -1,11 +1,20 @@
 package fr.ippon.tatami.repository.cassandra;
 
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import fr.ippon.tatami.config.ColumnFamilyKeys;
 import fr.ippon.tatami.repository.GroupCounterRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import javax.inject.Inject;
 import java.util.UUID;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.decr;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.incr;
 
 /**
  * Cassandra implementation of the Group Counter repository.
@@ -20,6 +29,8 @@ import java.util.UUID;
 @Repository
 public class CassandraGroupCounterRepository implements GroupCounterRepository {
 
+    @Inject
+    private Session session;
 
     @Override
     public long getGroupCounter(String domain, UUID groupId) {
@@ -37,20 +48,27 @@ public class CassandraGroupCounterRepository implements GroupCounterRepository {
 
     @Override
     public void incrementGroupCounter(String domain, UUID groupId) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.incrementCounter(domain, GROUP_COUNTER_CF, groupId, 1);
+        Statement statement = QueryBuilder.update(ColumnFamilyKeys.GROUP_COUNTER_CF)
+                .with(incr("counter",1))
+                .where(eq("domain",domain))
+                .and(eq("groupId", groupId));
+        session.execute(statement);
     }
 
     @Override
     public void decrementGroupCounter(String domain, UUID groupId) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.decrementCounter(domain, GROUP_COUNTER_CF, groupId, 1);
+        Statement statement = QueryBuilder.update(ColumnFamilyKeys.GROUP_COUNTER_CF)
+                .with(decr("counter",1))
+                .where(eq("domain",domain))
+                .and(eq("groupId", groupId));
+        session.execute(statement);
     }
 
     @Override
     public void deleteGroupCounter(String domain, String groupId) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.addCounterDeletion(domain, GROUP_COUNTER_CF, groupId, StringSerializer.get());
-//        mutator.execute();
+        Statement statement = QueryBuilder.delete().from(ColumnFamilyKeys.GROUP_COUNTER_CF)
+                .where(eq("domain", domain))
+                .and(eq("groupId",groupId));
+        session.execute(statement);
     }
 }
