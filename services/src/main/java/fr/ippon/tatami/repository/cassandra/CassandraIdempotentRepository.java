cc128309f971ea32dcdc21184fafe1dbf0549a2d
Lars Nørregaard <lnorregaard@nordija.com>
2015-11-30 11:35:19 +0100

    Replaced Cassandra code for Idempotent

--- services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraIdempotentRepository.java
+++ services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraIdempotentRepository.java
@@ -1,11 +1,21 @@
 package fr.ippon.tatami.repository.cassandra;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import fr.ippon.tatami.config.ColumnFamilyKeys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import fr.ippon.tatami.repository.IdempotentRepository;
 
 import javax.inject.Inject;
+import java.text.CollationKey;
+import java.util.UUID;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
 
 /**
  * Used to de-deplucate Camel messages.
@@ -21,6 +31,8 @@ public class CassandraIdempotentRepository implements IdempotentRepository {
 
     private final static int COLUMN_TTL = 60 * 60 * 24 * 30; // The column is stored for 30 days.
 
+    @Inject
+    private Session session;
 
     @Override
     public boolean add(String key) {
@@ -29,18 +41,10 @@ public class CassandraIdempotentRepository implements IdempotentRepository {
             return false;
         } else {
             log.debug("Adding new message to the idempotent repository");
-//            HColumn<String, String> column =
-//                    HFactory.createColumn(
-//                            key,
-//                            "",
-//                            COLUMN_TTL,
-//                            StringSerializer.get(),
-//                            StringSerializer.get());
-//
-//            Mutator<String> mutator =
-//                    HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//
-//            mutator.insert(KEY, TATAMIBOT_DUPLICATE_CF, column);
+            Statement statement = QueryBuilder.insertInto(ColumnFamilyKeys.TATAMIBOT_DUPLICATE_CF)
+                    .value(KEY, key)
+                    .using(QueryBuilder.ttl(COLUMN_TTL));
+            session.execute(statement);
             return true;
         }
     }
@@ -49,22 +53,20 @@ public class CassandraIdempotentRepository implements IdempotentRepository {
     public boolean contains(String key) {
 
         log.debug("Test message duplication with key : {}", key);
-//        ColumnQuery<String, String, String> query = HFactory.createStringColumnQuery(keyspaceOperator);
-//
-//        HColumn<String, String> column =
-//                query.setColumnFamily(TATAMIBOT_DUPLICATE_CF)
-//                        .setKey(KEY)
-//                        .setName(key)
-//                        .execute()
-//                        .get();
+        Statement statement = QueryBuilder.select()
+                .all()
+                .from(ColumnFamilyKeys.TATAMIBOT_DUPLICATE_CF)
+                .where(eq(KEY, key));
 
-        return false;
+        ResultSet results = session.execute(statement);
+        return !results.isExhausted();
     }
 
     @Override
     public boolean remove(String key) {
-//        Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//        mutator.delete(KEY, TATAMIBOT_DUPLICATE_CF, key, StringSerializer.get());
+        Statement statement = QueryBuilder.delete().from(TATAMIBOT_DUPLICATE_CF)
+                .where(eq(KEY, key));
+        session.execute(statement);
         return true;
     }
 
