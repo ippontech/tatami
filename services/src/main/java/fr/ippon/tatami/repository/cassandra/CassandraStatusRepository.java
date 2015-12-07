02fda5cf63689d0447e75be039954655723b5a87
Lars Nørregaard <lnorregaard@nordija.com>
2015-12-07 12:58:07 +0100

    Fixed problem with share status

--- services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraStatusRepository.java
+++ services/src/main/java/fr/ippon/tatami/repository/cassandra/CassandraStatusRepository.java
@@ -261,18 +261,6 @@ public class CassandraStatusRepository implements StatusRepository {
         return mentionShare;
     }
 
-
-    private Optional<Status> findOneFromIndex(BoundStatement stmt) {
-        ResultSet rs = session.execute(stmt);
-        if (rs.isExhausted()) {
-            return Optional.empty();
-        }
-        return Optional.ofNullable(rs.one().getUUID("statusId"))
-                .map(id -> Optional.ofNullable(mapper.get(id)))
-                .get();
-    }
-
-
     @Override
     @Cacheable("status-cache")
     public AbstractStatus findStatusById(String statusId) {
@@ -351,7 +339,7 @@ public class CassandraStatusRepository implements StatusRepository {
     private AbstractStatus findShare(Row result) {
         Share share = new Share();
         share.setType(StatusType.SHARE);
-        share.setOriginalStatusId(result.getString(ORIGINAL_STATUS_ID));
+        share.setOriginalStatusId(result.getUUID(ORIGINAL_STATUS_ID).toString());
         return share;
     }
 
