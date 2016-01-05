ba706d135e89aff1d49a264f5cd385896415e793
Leigha Wilson <j.leigha.wilson@gmail.com>
2016-01-05 14:58:24 -0500

    Tatami Mobile: Added Google Login front-end basics - still need to connect to login information

--- mobile/www/app/components/login/login.controller.js
+++ mobile/www/app/components/login/login.controller.js
@@ -1,3 +1,8 @@
+var requestToken = "";
+var accessToken = "";
+var clientId = "532238149370-40jujaon5evfgggvbeiocrcheaml6c4u.apps.googleusercontent.com";
+var clientSecret = "xFy65VlyGAcKz_QQoDVmoSDx";
+
 angular.module('tatami')
     .controller('LoginController', ['$scope', '$state', 'ProfileService', '$http', function($scope, $state, ProfileService, $http) {
         $scope.user = {};
@@ -13,5 +18,36 @@ angular.module('tatami')
                 $scope.failed = true;
                 console.log(error);
             });
+        };
+
+        // Google Login
+        $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+        $scope.googleLogin = function() {
+            var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=https://www.googleapis.com/auth/plus.login&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
+            ref.addEventListener('loadstart', function(event) {
+                if((event.url).startsWith("http://localhost/callback")) {
+                    requestToken = (event.url).split("code=")[1];
+                    $http({method: "post", url: "https://accounts.google.com/o/oauth2/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
+                        .success(function(data) {
+                            accessToken = data.access_token;
+                            $state.go('tab.dash');
+                        })
+                        .error(function(data, status) {
+                            alert("ERROR: " + data);
+                        });
+                    ref.close();
+                }
+            });
+        };
+
+        if (typeof String.prototype.startsWith != 'function') {
+            String.prototype.startsWith = function (str){
+                return this.indexOf(str) == 0;
+            };
         }
+
+
+
+
     }]);
