c99f48043882f8a8899c2ed37ee345578c8ed233
Kenny Hegeland <khegeland@ipponusa.com>
2016-01-05 12:51:07 -0500

    Removed commented code. Moved jetty to web, allowing debugging of backend code

--- mobile/www/app/components/login/login.controller.js
+++ mobile/www/app/components/login/login.controller.js
@@ -5,25 +5,6 @@ angular.module('tatami')
         $scope.failed = false;
 
         $scope.login = function() {
-            //$http({
-            //    method: 'POST',
-            //    url: '/tatami/authentication',
-            //    transformRequest: function(obj) {
-            //        var str = [];
-            //        for(var p in obj)
-            //            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            //        return str.join("&");
-            //    },
-            //    data: { j_username: $scope.user.email, j_password: $scope.user.password, _spring_security_remember_me: $scope.user.remember },
-            //    headers: { 'Content-Type': 'application/x-www-form-urlencoded'}
-            //}).success(function(data) {
-            //    $scope.shown = true;
-            //    console.log(data);
-            //    $state.go('tab.dash');
-            //}).error(function(err) {
-            //    $scope.failed = true;
-            //    console.log(err);
-            //});
             ProfileService.get(function(success) {
                 $scope.shown = true;
                 console.log(success);
