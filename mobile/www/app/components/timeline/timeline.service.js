908e337d9c7d910c03e73247cbfb44d9e263b0e7
Leigha Wilson <j.leigha.wilson@gmail.com>
2016-01-06 14:10:20 -0500

    Tatami Mobile: Update timeline look and feel to match current site more appropriately; setup view to pull in some more accurate data (still using test data - need to connect the correct services file)

--- mobile/www/app/components/timeline/timeline.service.js
+++ mobile/www/app/components/timeline/timeline.service.js
@@ -1,35 +1,91 @@
 angular.module('tatami')
 
     .factory('LineItems', function() {
-        // Might use a resource here that returns a JSON array
-
         // Some fake testing data
-        var lineItems = [{
-            id: 0,
-            name: 'Ben Sparrow',
-            lastText: 'You on your way?',
-            face: 'img/ben.png'
-        }, {
-            id: 1,
-            name: 'Max Lynx',
-            lastText: 'Hey, it\'s me',
-            face: 'img/max.png'
-        }, {
-            id: 2,
-            name: 'Adam Bradleyson',
-            lastText: 'I should buy a boat',
-            face: 'img/adam.jpg'
-        }, {
-            id: 3,
-            name: 'Perry Governor',
-            lastText: 'Look at my mukluks!',
-            face: 'img/perry.png'
-        }, {
-            id: 4,
-            name: 'Mike Harrington',
-            lastText: 'This is wicked good ice cream.',
-            face: 'img/mike.png'
-        }];
+        var lineItems = [
+            {
+                activated: true,
+                attachmentIds: null,
+                attachments: null,
+                avatar: "72ae6ec0-814c-11e5-ada7-fe54008898dc",
+                content: "Sur le salon #tatami ( https://chat.iaas.ippon.fr/channel/tatami ), vous pouvez désormais suivre en temps réel les mises à jour de votre réseau social préféré dans votre chat préféré 🐱",
+                detailsAvailable: true,
+                favorite: false,
+                firstName: "Pierre",
+                geoLocalization: "",
+                groupId: null,
+                groupName: null,
+                iso8601StatusDate: "2016-01-05T14:36:36.191+01:00",
+                lastName: "Baillet",
+                prettyPrintStatusDate: "5 Jan",
+                publicGroup: false,
+                replyTo: "",
+                replyToUsername: "",
+                shareByMe: false,
+                sharedByUsername: null,
+                statusDate: 1452000996191,
+                statusId: "576b47e0-b3b1-11e5-ada7-fe54008898dc",
+                statusPrivate: false,
+                timelineId: "576b47e0-b3b1-11e5-ada7-fe54008898dc",
+                type: "STATUS",
+                username: "pbaillet"
+            },
+            {
+                activated: true,
+                attachmentIds: null,
+                attachments: null,
+                avatar: "72ae6ec0-814c-11e5-ada7-fe54008898dc",
+                content: "Sur le salon vous pouvez désormais suivre en temps réel les mises à jour de votre réseau social préféré dans votre chat préféré",
+                detailsAvailable: true,
+                favorite: false,
+                firstName: "John",
+                geoLocalization: "",
+                groupId: null,
+                groupName: null,
+                iso8601StatusDate: "2015-02-06T15:03:26+01:00",
+                lastName: "Doe",
+                prettyPrintStatusDate: "6 Feb",
+                publicGroup: false,
+                replyTo: "",
+                replyToUsername: "",
+                shareByMe: false,
+                sharedByUsername: null,
+                statusDate: 1452000996191,
+                statusId: "576b47e0-b3b1-11e5-ada7-gf65127787cb",
+                statusPrivate: false,
+                timelineId: "576b47e0-b3b1-11e5-ada7-gf65127787cb",
+                type: "STATUS",
+                username: "jdoe"
+            },
+            {
+                activated: true,
+                attachmentIds: null,
+                attachments: null,
+                avatar: "72ae6ec0-814c-11e5-ada7-fe54008898dc",
+                content: "Sur le salon vous pouvez désormais suivre en temps réel les mises à jour de votre réseau social préféré dans votre chat préféré",
+                detailsAvailable: true,
+                favorite: false,
+                firstName: "Jane",
+                geoLocalization: "",
+                groupId: null,
+                groupName: null,
+                iso8601StatusDate: "2016-01-05T14:36:36.191+01:00",
+                lastName: "Bailey",
+                prettyPrintStatusDate: "5 Jan",
+                publicGroup: false,
+                replyTo: "",
+                replyToUsername: "",
+                shareByMe: false,
+                sharedByUsername: null,
+                statusDate: 1452000996191,
+                statusId: "576b47e0-b3b1-11e5-ada7-ab12345677cd",
+                statusPrivate: false,
+                timelineId: "576b47e0-b3b1-11e5-ada7-ab12345677cd",
+                type: "STATUS",
+                username: "jbailey"
+            }
+        ];
+
 
         return {
             all: function() {
@@ -40,8 +96,8 @@ angular.module('tatami')
             },
             get: function(lineItemId) {
                 for (var i = 0; i < lineItems.length; i++) {
-                    if (lineItems[i].id === parseInt(lineItemId)) {
-                        return mentions[i];
+                    if (lineItems[i].timelineId === lineItemId) {
+                        return lineItems[i];
                     }
                 }
                 return null;
