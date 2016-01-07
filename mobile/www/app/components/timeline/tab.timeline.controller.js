014c596b23ea02369816b2bae2438e774f8332ea
Leigha Wilson <j.leigha.wilson@gmail.com>
2016-01-07 16:42:08 -0500

    Tatami Mobile: Timeline is now using JSON from the site instead of harcoded arrays

--- mobile/www/app/components/timeline/tab.timeline.controller.js
+++ mobile/www/app/components/timeline/tab.timeline.controller.js
@@ -1,7 +1,19 @@
 angular.module('tatami')
-    .controller('TimelineCtrl', function ($scope, LineItems) {
+    .controller('TimelineCtrl', function ($scope, LineItems, $http) {
+
         $scope.lineItems = LineItems.all();
+        $scope.lineItems.success(function(data) {
+            $scope.lineItems = data;
+        });
         $scope.remove = function (lineItem) {
             LineItems.remove(lineItem);
+        };
+
+        $scope.getAvatarURL = function(avatar) {
+            var src = '../../../img/test-prof.png';
+            if( avatar != '' &&  avatar != undefined) {
+                src = '/tatami/avatar/' + avatar + '/photo.jpg';
+            }
+            return src;
         };
     });
