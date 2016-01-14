22af5ab6cdc3bf9024bd39e4fc82128ad08f4c5a
Kenny Hegeland <khegeland@ipponusa.com>
2016-01-14 10:57:02 -0500

    Improved the look of user display in the tatami user directive

--- mobile/www/app/shared/user/users.directive.js
+++ mobile/www/app/shared/user/users.directive.js
@@ -1,22 +1,36 @@
-angular.module('tatami')
-    .directive('tatamiUsers', ['$state', function($state) {
-        return {
+(function() {
+    'use strict';
+
+    angular.module('tatami')
+        .directive('tatamiUser', tatamiUser);
+
+    tatamiUser.$inject = ['$state'];
+    function tatamiUser($state) {
+        var directive = {
             restrict: 'E',
-            replace: true,
             scope: {
                 user: '='
             },
-            templateUrl: 'app/shared/user/users.html',
-            controller: ['$scope', '$state', 'UserService', function($scope, $state, UserService) {
-                $scope.followUser = function() {
-                    UserService.follow({ username : $scope.user.username }, { friend: !$scope.user.friend, friendShip: true },
-                        function() {
-                            $state.reload();
-                        });
-                }
-            }],
-            link: function(scope, el, attrs) {
-            }
+            controller: controller,
+            controllerAs: 'vm',
+            templateUrl: 'app/shared/user/users.html'
+        };
+
+        return directive;
+    }
+
+    controller.$inject = ['$scope', '$state', 'UserService'];
+    function controller($scope, $state, UserService) {
+        var vm = this;
+
+        vm.user = $scope.user;
+        vm.followUser = followUser;
+        function followUser() {
+            UserService.follow({ username : vm.user.username }, { friend: !vm.user.friend, friendShip: true },
+                function() {
+                    $state.reload();
+                });
         }
-    }]
-);
+
+    }
+})();
