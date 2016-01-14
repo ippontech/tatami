8e866c4a0ef8672b098d9cac90662517490ef47a
Jacob Holcombe <holcombejb@vcu.edu>
2016-01-14 15:50:37 -0500

    added the Tatami login User Interface

--- src/main/webapp/scripts/app/account/login/login.js
+++ src/main/webapp/scripts/app/account/login/login.js
@@ -7,7 +7,7 @@ angular.module('tatamiJHipsterApp')
                 parent: 'account',
                 url: '/login',
                 data: {
-                    authorities: [], 
+                    authorities: [],
                     pageTitle: 'login.title'
                 },
                 views: {
