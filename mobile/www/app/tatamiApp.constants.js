6ffcacb33b9ff1b054faba8e876995074e2e100f
Kenny Hegeland <khegeland@ipponusa.com>
2016-01-21 13:56:57 -0500

    Added follow main header to the navbar header

--- mobile/www/app/tatamiApp.constants.js
+++ mobile/www/app/tatamiApp.constants.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    var endpointConstant = { url: 'http://10.1.10.201:8080' };
+    var endpointConstant = { url: 'http://10.1.10.201:8100' };
 
     angular.module('tatami')
         .constant('TatamiEndpoint', endpointConstant);
