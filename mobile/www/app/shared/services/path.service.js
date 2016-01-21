d39b20d98dbaaa87a8ca438802de51bb26e8c78c
Kenny Hegeland <khegeland@ipponusa.com>
2016-01-21 12:53:12 -0500

    Added service which will generate urls for avatars and rest requests by prepending the endpoint url

---
+++ mobile/www/app/shared/services/path.service.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    angular.module('tatami')
+        .factory('PathService', avatarService);
+
+    avatarService.$inject = ['TatamiEndpoint'];
+    function avatarService(TatamiEndpoint) {
+        var service = {
+            getAvatar: getAvatar,
+            buildPath: buildPath
+        };
+
+        return service;
+
+        getAvatar.$inject = ['user'];
+        function getAvatar(user) {
+            return TatamiEndpoint.url + (user.avatar === '' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + user.avatar + '/photo.jpg');
+        }
+
+        buildPath.$inject = ['resourcePath'];
+        function buildPath(resourcePath) {
+            return TatamiEndpoint.url + resourcePath;
+        }
+    }
+})();
