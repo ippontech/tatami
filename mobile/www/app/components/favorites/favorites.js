b310fffc09ea61cbf161b85987586d906e110ca7
Kenny Hegeland <khegeland@ipponusa.com>
2016-01-25 17:36:46 -0500

    Set up routing for favorites timeline view

--- mobile/www/app/components/favorites/favorites.js
+++ mobile/www/app/components/favorites/favorites.js
@@ -1,14 +1,23 @@
-angular.module('tatami')
-    .config(function ($stateProvider, $urlRouterProvider) {
+(function() {
+    'use strict';
 
+    angular.module('tatami')
+        .config(config);
+
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
         $stateProvider
             .state('tab.favorites', {
                 url: '/favorites',
                 views: {
                     'tab-favorites': {
                         templateUrl: 'app/components/favorites/tab-favorites.html',
-                        controller: 'FavoritesCtrl'
+                        controller: 'FavoritesCtrl',
+                        controllerAs: 'vm'
                     }
+                },
+                resolve: {
+                    favorites: favorites
                 }
             })
             .state('tab.favorites-detail', {
@@ -20,5 +29,10 @@ angular.module('tatami')
                     }
                 }
             });
+
+        favorites.$inject = ['HomeService'];
+        function favorites(HomeService) {
+            return HomeService.getFavorites().$promise;
+        }
     }
-);
+})();
