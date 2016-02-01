234d325685a8a8a92cfc9bfccb552df0483378f6
Kenny Hegeland <khegeland@ipponusa.com>
2016-02-01 14:20:02 -0500

    Rewrite the post module to use better code style. Added ability to specify the status you are replying to via the state

--- mobile/www/app/components/post/post.controller.js
+++ mobile/www/app/components/post/post.controller.js
@@ -1,25 +1,35 @@
-angular.module('tatami')
-    .controller('PostCtrl', ['$scope', '$state', '$ionicHistory', 'StatusService', function($scope, $state, $ionicHistory, StatusService) {
-        $scope.charCount = 750;
-        $scope.status = {
-            content: '',
-            statusPrivate: false
+(function() {
+    'use strict';
+
+    angular.module('tatami')
+        .controller('PostCtrl', postCtrl);
+
+    postCtrl.$inject = ['StatusService', '$ionicHistory', '$state', 'repliedToStatus'];
+    function postCtrl(StatusService, $ionicHistory, $state, repliedToStatus) {
+        var vm = this;
+        vm.charCount = 750;
+        vm.status = {
+            content: repliedToStatus ? '@' + repliedToStatus.username : '',
+            statusPrivate: repliedToStatus ? repliedToStatus.private : false
         };
 
-        $scope.post = function() {
-            StatusService.save($scope.status, function() {
-                $scope.reset();
+        vm.post = post;
+        vm.reset = reset;
+
+        function post() {
+            console.log(StatusService);
+            StatusService.save(vm.status, function() {
+                reset();
                 $ionicHistory.clearCache();
                 $state.go('timeline');
             })
-        };
+        }
 
-        $scope.reset = function() {
-            $scope.status = {
+        function reset() {
+            vm.status = {
                 content: '',
                 statusPrivate: false
             }
         }
-
-    }]
-);
+    }
+})();
--- mobile/www/app/components/post/post.js
+++ mobile/www/app/components/post/post.js
@@ -1,12 +1,28 @@
-angular.module('tatami')
-    .config(function ($stateProvider, $urlRouterProvider) {
+(function() {
+    'use strict';
 
+    angular.module('tatami')
+        .config(postConfig);
+
+    postConfig.$inject = ['$stateProvider'];
+    function postConfig($stateProvider) {
         $stateProvider
             .state('post', {
-                url: '/post',
+                url: '/post/:statusId',
                 parent: 'tatami',
                 templateUrl: 'app/components/post/post.html',
-                controller: 'PostCtrl'
-            });
+                controller: 'PostCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    repliedToStatus: getRepliedToStatus
+                }
+            })
     }
-);
+
+    getRepliedToStatus.$inject = ['StatusService', '$stateParams'];
+    function getRepliedToStatus(StatusService, $stateParams) {
+        if($stateParams.statusId) {
+            return StatusService.get({ statusId: $stateParams.statusId }).$promise;
+        }
+    }
+})();
