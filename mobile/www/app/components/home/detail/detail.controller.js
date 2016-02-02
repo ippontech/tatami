13766d2c4604769316da8bb9a7a415129cda736e
Kenny Hegeland <khegeland@ipponusa.com>
2016-02-02 12:51:05 -0500

    Generic controller and template for displaying the details of a status Deleted detail template in timeline and mention directory in favor of the generic ones

---
+++ mobile/www/app/components/home/detail/detail.controller.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular.module('tatami')
+        .controller('DetailCtrl', detailCtrl);
+
+    detailCtrl.$inject = ['status'];
+    function detailCtrl(status) {
+        var vm = this;
+
+        vm.status = status;
+        console.log(vm.status);
+    }
+})();
--- mobile/www/app/components/home/mentions/mentions.detail.controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('tatami')
-    .controller('MentionDetailCtrl', function ($scope, $stateParams, Mentions) {
-        $scope.mention = Mentions.get($stateParams.mentionId);
-    });
