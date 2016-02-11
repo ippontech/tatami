b3f9c3018e4cce750edd7c7cd89a0deabf16bfa3
Kenny Hegeland <khegeland@ipponusa.com>
2016-02-11 11:36:47 -0500

    Added status substate for favorites tab

--- mobile/www/app/components/home/favorites/favorites.js
+++ mobile/www/app/components/home/favorites/favorites.js
@@ -20,29 +20,19 @@
                 resolve: {
                     favorites: favorites
                 }
-            })
-            .state('favorites.detail', {
-                url: '/detail/:statusId',
-                views: {
-                    'favorites@home': {
-                        templateUrl: 'app/components/home/detail/detail.html',
-                        controller: 'DetailCtrl',
-                        controllerAs: 'vm'
-                    }
-                },
-                resolve: {
-                    status: getStatus
-                }
             });
 
         favorites.$inject = ['HomeService'];
         function favorites(HomeService) {
             return HomeService.getFavorites().$promise;
         }
+    }
 
-        getStatus.$inject = ['StatusService', '$stateParams'];
-        function getStatus(StatusService, $stateParams) {
-            return StatusService.get({ statusId : $stateParams.statusId }).$promise;
-        }
+    angular.module('tatami')
+        .run(run);
+
+    run.$inject = ['TatamiState'];
+    function run(TatamiState) {
+        TatamiState.addStatusState('favorites', 'home');
     }
 })();
