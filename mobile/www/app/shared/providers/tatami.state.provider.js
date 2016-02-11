60fd83c20499d6196a6d4171a761a533b1eb7251
Kenny Hegeland <khegeland@ipponusa.com>
2016-02-11 11:36:19 -0500

    Chaned StatusState to TatamiState. This handles creating profile and status substates at runtime

--- mobile/www/app/shared/providers/tatami.state.provider.js
+++ mobile/www/app/shared/providers/tatami.state.provider.js
@@ -4,33 +4,33 @@
     // This will dynamically create any tab substates inside the current tab. If in the timeline tab, we will
     // create a timeline.status state
     angular.module('tatami.providers')
-        .provider('StatusState', statusState);
+        .provider('TatamiState', tatamiState);
 
-    statusState.$inject = ['$stateProvider'];
-    function statusState($stateProvider) {
-        this.$get = StatusStateHelper;
+    tatamiState.$inject = ['$stateProvider'];
+    function tatamiState($stateProvider) {
+        this.$get = tatamiStateHelper;
 
-        function StatusStateHelper() {
+        function tatamiStateHelper() {
 
-            var viewConfig = {
+            var statusViewConfig = {
                 templateUrl: 'app/shared/state/status/status.html',
                 controller: 'StatusCtrl',
                 controllerAs: 'vm'
             };
 
             var views = [];
-            views['timeline@home'] = { 'timeline@home': viewConfig };
-            views['mentions@home'] = { 'mentions@home': viewConfig };
-            views['favorites@home'] = { 'favorites@home': viewConfig };
+            views['timeline@home'] = { 'timeline@home': statusViewConfig };
+            views['mentions@home'] = { 'mentions@home': statusViewConfig };
+            views['favorites@home'] = { 'favorites@home': statusViewConfig };
 
             var service = {
-                addStatusState: addStatusState
+                addStatusState: addStatusState,
+                addProfileState: addProfileState
             };
 
             return service;
 
             function addStatusState(prefixName, parentName) {
-                console.log(views['timeline@home']);
                 var viewName = prefixName + '@' + parentName;
                 $stateProvider.state(prefixName + '.status', {
                     url: '/status/:statusId',
@@ -44,6 +44,10 @@
                 function getStatus($stateParams, StatusService) {
                     return StatusService.get({ statusId : $stateParams.statusId }).$promise;
                 }
+            }
+
+            function addProfileState(prefixName, parentName) {
+
             }
 
 
