ba58b0d5506c9de70416310ef4d7286f0ed66d5b
Kenny Hegeland <khegeland@ipponusa.com>
2016-02-25 23:33:18 -0500

    Get authorization code, then call rest endpoint to start authentication process

--- mobile/www/app/components/login/login.controller.js
+++ mobile/www/app/components/login/login.controller.js
@@ -2,14 +2,13 @@
     'use strict';
     var requestToken = "";
     var accessToken = "";
-    var clientId = "532238149370-40jujaon5evfgggvbeiocrcheaml6c4u.apps.googleusercontent.com";
-    var clientSecret = "xFy65VlyGAcKz_QQoDVmoSDx";
+    var clientId = "260089373157-5a6n36mg1vgarq66ph3t58ipt70a5d3l.apps.googleusercontent.com";
 
     angular.module('tatami')
         .controller('LoginCtrl', loginCtrl);
 
-    loginCtrl.$inject = ['$scope', '$state', 'LoginService'];
-    function loginCtrl($scope, $state, LoginService) {
+    loginCtrl.$inject = ['$scope', '$state', '$http', 'LoginService'];
+    function loginCtrl($scope, $state, $http, LoginService) {
         var vm = this;
         vm.user = {
             remember: false
@@ -33,32 +32,53 @@
         }
 
         function googleLogin() {
-            var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost:8080/tatami/callback?client_name=Google2Client&scope=https://www.googleapis.com/auth/plus.login&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
-            ref.addEventListener('loadstart', function(event) {
+            var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
+
+            ref.addEventListener('loadstart', onStart);
+            onStart.$inject = ['event'];
+            function onStart(event) {
                 if((event.url).startsWith("http://localhost/callback")) {
-                    requestToken = (event.url).split("code=")[1];
+                    var requestToken = (event.url).split("code=")[1];
+                    console.log(requestToken);
+
                     $http({
-                        method: "post",
-                        url: "https://accounts.google.com/o/oauth2/token",
-                        data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost:8080/tatami/callback?client_name=Google2Client" + "&grant_type=authorization_code" + "&code=" + requestToken,
-                        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-                    })
-                        .success(function(data) {
-                            accessToken = data.access_token;
-                            $state.go('tab.timeline');
-                        })
-                        .error(function(data, status) {
-                            alert("ERROR: " + data);
-                        });
-                    ref.close();
+                        url: '/tatami/rest/oauth/token',
+                        method: 'POST',
+                        headers: {
+                            'x-auth-token': requestToken
+                        }
+                    });
                 }
-            });
+            }
         }
 
-        if (typeof String.prototype.startsWith != 'function') {
-            String.prototype.startsWith = function (str){
-                return this.indexOf(str) == 0;
-            };
-        }
+        //function googleLogin() {
+        //    var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost:8080/tatami/callback?client_name=Google2Client&scope=https://www.googleapis.com/auth/plus.login&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no');
+        //    ref.addEventListener('loadstart', function(event) {
+        //        if((event.url).startsWith("http://localhost/callback")) {
+        //            requestToken = (event.url).split("code=")[1];
+        //            $http({
+        //                method: "post",
+        //                url: "https://accounts.google.com/o/oauth2/token",
+        //                data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost:8080/tatami/callback?client_name=Google2Client" + "&grant_type=authorization_code" + "&code=" + requestToken,
+        //                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        //            })
+        //                .success(function(data) {
+        //                    accessToken = data.access_token;
+        //                    $state.go('tab.timeline');
+        //                })
+        //                .error(function(data, status) {
+        //                    alert("ERROR: " + data);
+        //                });
+        //            ref.close();
+        //        }
+        //    });
+        //}
+        //
+        //if (typeof String.prototype.startsWith != 'function') {
+        //    String.prototype.startsWith = function (str){
+        //        return this.indexOf(str) == 0;
+        //    };
+        //}
     }
 })();
