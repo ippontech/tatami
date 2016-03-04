287233621d423c632be1a803975548434d171096
Derek Zuk <dzuk@ippon.fr>
2016-03-04 11:04:37 -0500

    Added angular-moment to help with displaying dates on timeline posts.

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -4,7 +4,7 @@ angular.module('tatamiJHipsterApp', ['LocalStorageModule', 'tmh.dynamicLocale', 
     'ngResource', 'ngCookies', 'ngAria', 'ngCacheBuster', 'ngFileUpload',
     // jhipster-needle-angularjs-add-module JHipster will add new module
     'ui.bootstrap', 'ui.router',  'infinite-scroll', 'angular-loading-bar',
-    'ngToast'])
+    'ngToast', 'angularMoment'])
 
     .run(function ($rootScope, $location, $window, $http, $state, $translate, Language, Auth, Principal, ENV, VERSION) {
         // update the window title using params in the following
