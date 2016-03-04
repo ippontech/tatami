5e11d3d066d59861a7927f156949daa19bf773fa
Kenny Hegeland <khegeland@ipponusa.com>
2016-03-04 12:49:37 -0500

    Add ion-infinite-scroll to the status list

--- mobile/www/app/shared/status/list/status.list.directive.js
+++ mobile/www/app/shared/status/list/status.list.directive.js
@@ -10,7 +10,8 @@
             scope: {
                 statuses: '=',
                 currentUser: '=',
-                tatamiRefresher: '&'
+                tatamiRefresher: '&',
+                tatamiInfiniteRefresher: '&'
             },
             controller: controller,
             controllerAs: 'vm',
@@ -26,10 +27,16 @@
         vm.statuses = $scope.statuses;
         vm.currentUser = $scope.currentUser;
         vm.getNewStatuses = getNewStatuses;
+        vm.getNewInfiniteScrollStatuses = getNewInfiniteScrollStatuses;
         vm.remove = remove;
+        vm.finishedTimeline = true;
 
         function getNewStatuses() {
             $scope.tatamiRefresher().then(setStatuses);
+        }
+
+        function getNewInfiniteScrollStatuses() {
+            $scope.tatamiInfiniteRefresher({ finalStatus: vm.statuses[vm.statuses.length - 1].timelineId }).then(addNewStatuses);
         }
 
         remove.$inject = ['status'];
@@ -40,6 +47,15 @@
         setStatuses.$inject = ['statuses'];
         function setStatuses(statuses) {
             vm.statuses = statuses;
+        }
+
+        addNewStatuses.$inject = ['oldStatuses'];
+        function addNewStatuses(oldStatuses) {
+            if(oldStatuses.length == 0) {
+                vm.finishedTimeline = false;
+            }
+            
+            vm.statuses.push.apply(vm.statuses, oldStatuses);
         }
     }
 })();
