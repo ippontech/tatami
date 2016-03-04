56454387b606f55f857010056cb44c45fc8134cb
Kenny Hegeland <khegeland@ipponusa.com>
2016-03-04 13:34:08 -0500

    Only 50 posts can be favorited, and they are all fetched at the same time. So infinite scrolling is not needed, return an empty array, which will hide the ion-infinite-scroll

--- mobile/www/app/components/home/favorites/favorites.controller.js
+++ mobile/www/app/components/home/favorites/favorites.controller.js
@@ -4,15 +4,24 @@
     angular.module('tatami')
         .controller('FavoritesCtrl', favoritesCtrl);
 
-    favoritesCtrl.$inject = ['favorites', 'currentUser', 'TatamiStatusRefresherService'];
-    function favoritesCtrl(favorites, currentUser, TatamiStatusRefresherService) {
+    favoritesCtrl.$inject = ['favorites', 'currentUser', 'TatamiStatusRefresherService', '$q'];
+    function favoritesCtrl(favorites, currentUser, TatamiStatusRefresherService, $q) {
         var vm = this;
         vm.favorites = favorites;
         vm.currentUser = currentUser;
         vm.getNewStatuses = getNewStatuses;
+        vm.getEmpty = getEmpty;
 
         function getNewStatuses() {
             return TatamiStatusRefresherService.refreshFavorites();
+        }
+
+        getEmpty.$inject = ['finalStatus'];
+        function getEmpty(finalStatus) {
+            console.log('testing');
+            var deferred = $q.defer();
+            deferred.resolve([]);
+            return deferred.promise;
         }
     }
 })();
