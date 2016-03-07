df0d85b194e4f70d22580a9cd4c4ee327f5f89d3
Derek Zuk <dzuk@ippon.fr>
2016-03-07 11:41:44 -0500

    Cleanup location of default avatar img. Cleanup duplicate services.

--- src/main/webapp/scripts/components/auth/services/status.service.js
+++ src/main/webapp/scripts/components/auth/services/status.service.js
@@ -4,8 +4,7 @@ angular.module('tatamiJHipsterApp')
         statuses = angular.fromJson(statuses);
 
         for(var i = 0; i < statuses.length; i++) {
-            statuses[i]['avatarURL'] = statuses[i].avatar==='' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + statuses[i].avatar + '/photo.jpg';
-
+            statuses[i]['avatarURL'] = statuses[i].avatar===null ? '/assets/images/default_image_profile.png' : '/tatami/avatar/' + statuses[i].avatar + '/photo.jpg';
             if(statuses[i].geoLocalization) {
                 var latitude = statuses[i].geoLocalization.split(',')[0].trim();
                 var longitude = statuses[i].geoLocalization.split(',')[1].trim();
@@ -25,7 +24,7 @@ angular.module('tatamiJHipsterApp')
             transformResponse: function(status) {
                 status = angular.fromJson(status);
 
-                status.avatarURL = status.avatar==='' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + status.avatar + '/photo.jpg';
+                status.avatarURL = status.avatar==='' ? '/assets/images/default_image_profile.png' : '/tatami/avatar/' + status.avatar + '/photo.jpg';
 
                 if(status.geoLocalization) {
                     var latitude = status.geoLocalization.split(',')[0].trim();
@@ -52,7 +51,7 @@ angular.module('tatamiJHipsterApp')
                 details = angular.fromJson(details);
 
                 for(var i = 0; i < details.discussionStatuses.length; i++) {
-                    details.discussionStatuses[i]['avatarURL'] = details.discussionStatuses[i].avatar==='' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + details.discussionStatuses[i].avatar + '/photo.jpg';
+                    details.discussionStatuses[i]['avatarURL'] = details.discussionStatuses[i].avatar==='' ? '/assets/images/default_image_profile.png' : '/tatami/avatar/' + details.discussionStatuses[i].avatar + '/photo.jpg';
 
                     if(details.discussionStatuses[i].geoLocalization) {
                         var latitude = details.discussionStatuses[i].geoLocalization.split(',')[0].trim();
@@ -64,7 +63,7 @@ angular.module('tatamiJHipsterApp')
                 }
 
                 for(var i = 0; i < details.sharedByLogins.length; i++) {
-                    details.sharedByLogins[i]['avatarURL'] = details.sharedByLogins[i].avatar==='' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + details.sharedByLogins[i].avatar + '/photo.jpg';
+                    details.sharedByLogins[i]['avatarURL'] = details.sharedByLogins[i].avatar==='' ? '/assets/images/default_image_profile.png' : '/tatami/avatar/' + details.sharedByLogins[i].avatar + '/photo.jpg';
                 }
 
                 return details;
--- src/main/webapp/scripts/shared/services/SearchService.js
+++ src/main/webapp/scripts/shared/services/SearchService.js
@@ -4,7 +4,7 @@ tatamiJHipsterApp
         users = angular.fromJson(users);
 
         for(var i = 0; i < users.length; i++) {
-            users[i]['avatarURL'] = users[i].avatar==='' ? '/assets/img/default_image_profile.png' : '/tatami/avatar/' + users[i].avatar + '/photo.jpg';
+            users[i]['avatarURL'] = users[i].avatar==='' ? '/assets/images/default_image_profile.png' : '/tatami/avatar/' + users[i].avatar + '/photo.jpg';
         }
 
         return users;
@@ -12,7 +12,7 @@ tatamiJHipsterApp
 
     return $resource('/tatami/rest/search/:term', { term: '@term' },
     {
-        'query': { 
+        'query': {
             method: 'GET', isArray: true, transformResponse: responseTransform
         },
         'get': {
