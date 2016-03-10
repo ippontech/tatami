27c6865ef1b4b7e9683ad793c59b09b2c8d1d155
kreinerjm <kreinerjm@hotmail.com>
2016-03-10 15:38:48 -0500

    Updated Navbar to hide functionality in certain states. Added logout button on navbar when in account state. Added translation field for logout button. Replaced profile.username with profile.login in homeSidebar.html

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -4,6 +4,8 @@ tatamiJHipsterApp
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal, ENV, PostModalService) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.inProduction = ENV === 'prod';
+        $scope.$state = $state;
+        console.log($state.$current);
         $scope.logout = function () {
             Auth.logout();
             $state.go('home');
