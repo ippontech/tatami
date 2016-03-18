087588d586ca07823b58f406c8a7eb6beb7f7076
Kenny Hegeland <khegeland@ipponusa.com>
2016-03-18 15:47:29 -0400

    The id for a user is a string

--- src/main/java/fr/ippon/tatami/repository/search/UserSearchRepository.java
+++ src/main/java/fr/ippon/tatami/repository/search/UserSearchRepository.java
@@ -3,5 +3,5 @@ package fr.ippon.tatami.repository.search;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import fr.ippon.tatami.domain.User;
 
-public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+public interface UserSearchRepository extends ElasticsearchRepository<User, String> {
 }
