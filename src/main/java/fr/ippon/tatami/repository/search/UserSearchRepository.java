7c1eeb0ec9b2776f0ca9f14527d725f1da1dfa13
Kenny Hegeland <khegeland@ipponusa.com>
2016-03-18 15:29:34 -0400

    Add search repository for users

---
+++ src/main/java/fr/ippon/tatami/repository/search/UserSearchRepository.java
@@ -0,0 +1,7 @@
+package fr.ippon.tatami.repository.search;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import fr.ippon.tatami.domain.User;
+
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
