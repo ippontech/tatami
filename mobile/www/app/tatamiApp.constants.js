0161e3a1b584c82f6c148125752590a7432494f7
Kenny Hegeland <khegeland@ipponusa.com>
2016-03-22 09:50:16 -0400

    Update endpoint

--- mobile/www/app/tatamiApp.constants.js
+++ mobile/www/app/tatamiApp.constants.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    var endpointConstant = { url: 'http://10.1.10.202:8100' };
+    var endpointConstant = { url: 'http://app.tatamisoft.com' };
 
     angular.module('tatami')
         .constant('TatamiEndpoint', endpointConstant);
