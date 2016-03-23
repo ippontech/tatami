d56dc76681ba208271de0783aeca0d35dfcf0793
Nathan Coates <ncoates@ipponusa.com>
2016-03-23 12:12:36 -0400

    temporarily put transformResponse back into profile controller

--- src/main/webapp/scripts/components/auth/services/profile.service.js
+++ src/main/webapp/scripts/components/auth/services/profile.service.js
@@ -5,7 +5,17 @@ tatamiJHipsterApp
         return $resource('/tatami/rest/account/profile', null,
             {
                 'get': {
-                    method: 'GET'
+                    method: 'GET',
+                    transformResponse: function (profile) {
+                        var parsedProfile = {};
+                        try {
+                            parsedProfile = angular.fromJson(profile);
+                        } catch(e) {
+                            parsedProfile = {};
+                        }
+                        parsedProfile['avatarURL'] = parsedProfile.avatar ==='' ? '/assets/images/default_image_profile.png' : '/tatami/avatar/' + parsedProfile.avatar + '/photo.jpg';
+                        return parsedProfile;
+                    }
                 },
                 'update': {
                     method: 'POST',
