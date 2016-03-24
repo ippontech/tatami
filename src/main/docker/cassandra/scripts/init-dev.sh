2415e563d110a0d4fcb709690476f3d4602d3358
Raphael Brugier <raphael.brugier@gmail.com>
2016-03-24 11:04:15 -0400

    add a third container to automatically apply database scripts in Dev environment

--- src/main/docker/cassandra/scripts/init-dev.sh
+++ src/main/docker/cassandra/scripts/init-dev.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+function log {
+    echo "[$(date)]: $*"
+}
+
 cat /cql/create-keyspace.cql > create-keyspace-tables.cql
 echo "USE TatamiJHipster;" >> create-keyspace-tables.cql
 cat /cql/create-tables.cql >> create-keyspace-tables.cql
 
-cqlsh -f create-keyspace-tables.cql
+retryCount=0
+maxRetry=20
+cqlsh -e "Describe KEYSPACES;" tatami-cassandra &>/dev/null
+while [ $? -ne 0 ] && [ "$retryCount" -ne "$maxRetry" ]; do
+    log 'cassandra not reachable yet. sleep and retry. retryCount =' $retryCount
+    sleep 5
+    ((retryCount+=1))
+    cqlsh -e "Describe KEYSPACES;" tatami-cassandra &>/dev/null
+done
+
+if [ $? -eq 0 ]; then
+  log "connected to cassandra cluster"
+else
+  log "not connected after " $retryCount " retry. Abort the migration."
+  exit 1
+fi
+
+log "launch migration script"
+cqlsh -f create-keyspace-tables.cql --connect-timeout=60 tatami-cassandra
+
+if [ $? -eq 0 ]; then
+  log "migration done"
+  exit 0
+else
+  log "migration failed"
+  exit 1
+fi
