ce0603a84ea30026328a4d24d58217c51421978f
Raphael Brugier <raphael.brugier@gmail.com>
2016-03-28 17:30:21 -0400

    clean init dockerized Cassandra script

--- src/main/docker/cassandra/scripts/init-dev.sh
+++ src/main/docker/cassandra/scripts/init-dev.sh
@@ -3,61 +3,61 @@
 # Protect from iterating on empty directories
 shopt -s nullglob
 
+CASSANDRA_CONTACT_POINT="tatami-cassandra"
+KEYSPACE_NAME="TatamiJHipster"
+
 function log {
     echo "[$(date)]: $*"
 }
 
-retryCount=0
-maxRetry=20
-cqlsh -e "Describe KEYSPACES;" tatami-cassandra &>/dev/null
-while [ $? -ne 0 ] && [ "$retryCount" -ne "$maxRetry" ]; do
-    log 'cassandra not reachable yet. sleep and retry. retryCount =' $retryCount
-    sleep 5
-    ((retryCount+=1))
-    cqlsh -e "Describe KEYSPACES;" tatami-cassandra &>/dev/null
-done
+function waitForClusterConnection() {
+    retryCount=0
+    maxRetry=20
+    cqlsh -e "Describe KEYSPACES;" $CASSANDRA_CONTACT_POINT &>/dev/null
+    while [ $? -ne 0 ] && [ "$retryCount" -ne "$maxRetry" ]; do
+        log 'cassandra not reachable yet. sleep and retry. retryCount =' $retryCount
+        sleep 5
+        ((retryCount+=1))
+        cqlsh -e "Describe KEYSPACES;" $CASSANDRA_CONTACT_POINT &>/dev/null
+    done
 
-if [ $? -eq 0 ]; then
-  log "connected to cassandra cluster"
-else
-  log "not connected after " $retryCount " retry. Abort the migration."
-  exit 1
-fi
+    if [ $? -eq 0 ]; then
+      log "connected to cassandra cluster"
+    else
+      log "not connected after " $retryCount " retry. Abort the migration."
+      exit 1
+    fi
+}
+
+function executeScripts() {
+    local filePattern=$1
+
+    # loop over migration scripts
+    for cqlFile in $filePattern; do
+        filename=${cqlFile##*/}
+        log "execute: " $filename
+        echo "USE $KEYSPACE_NAME;" > $filename
+        cat $cqlFile >> $filename
+        cqlsh -f $filename $CASSANDRA_CONTACT_POINT
+        if [ $? -ne 0 ]; then
+            log "fail to apply script " $filename
+            log "stop applying database changes"
+            exit 1
+        fi
+    done
+}
+
+waitForClusterConnection
 
 log "execute migration scripts"
 
 log "create keyspace and base tables"
 cat /cql/create-keyspace.cql > create-keyspace-tables.cql
-echo "USE TatamiJHipster;" >> create-keyspace-tables.cql
+echo "USE $KEYSPACE_NAME;" >> create-keyspace-tables.cql
 cat /cql/create-tables.cql >> create-keyspace-tables.cql
-cqlsh -f create-keyspace-tables.cql tatami-cassandra
+cqlsh -f create-keyspace-tables.cql $CASSANDRA_CONTACT_POINT
 
-# loop over new entities scripts
-for cqlFile in /cql/*_added_entity_*.cql; do
-    filename=${cqlFile##*/}
-    log "execute: " $filename
-    echo "USE TatamiJHipster;" > $filename
-    cat $cqlFile >> $filename
-    cqlsh -f $filename tatami-cassandra
-    if [ $? -ne 0 ]; then
-        log "fail to apply script " $filename
-        log "stop applying database changes"
-        exit 1
-    fi
-done
-
-# loop over migration scripts
-for cqlFile in /cql/migration/V*.cql; do
-    filename=${cqlFile##*/}
-    log "execute: " $filename
-    echo "USE TatamiJHipster;" > $filename
-    cat $cqlFile >> $filename
-    cqlsh -f $filename tatami-cassandra
-    if [ $? -ne 0 ]; then
-        log "fail to apply script " $filename
-        log "stop applying database changes"
-        exit 1
-    fi
-done
+executeScripts /cql/*_added_entity_*.cql
+executeScripts /cql/migration/V*.cql
 
 log "migration done"
