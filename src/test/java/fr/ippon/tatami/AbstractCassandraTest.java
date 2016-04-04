90ff6330c418809be1c34fd57aa29337a8754f86
Raphael Brugier <raphael.brugier@gmail.com>
2016-04-04 09:44:09 -0400

    correct exception handling in CassandraAbstract test

--- src/test/java/fr/ippon/tatami/AbstractCassandraTest.java
+++ src/test/java/fr/ippon/tatami/AbstractCassandraTest.java
@@ -22,28 +22,24 @@ public class AbstractCassandraTest {
     public static final String CASSANDRA_UNIT_KEYSPACE = "cassandra_unit_keyspace";
 
     @BeforeClass
-    public static void startServer() throws InterruptedException, TTransportException, ConfigurationException, IOException {
+    public static void startServer() throws InterruptedException, TTransportException, ConfigurationException, IOException, URISyntaxException {
         EmbeddedCassandraServerHelper.startEmbeddedCassandra();
         Cluster cluster = new Cluster.Builder().addContactPoints("127.0.0.1").withPort(9142).build();
         Session session = cluster.connect();
         CQLDataLoader dataLoader = new CQLDataLoader(session);
         dataLoader.load(new ClassPathCQLDataSet("config/cql/create-tables.cql", true, "cassandra_unit_keyspace"));
 
-        applyScripts(dataLoader, "config/cql/", "*_added_entity_*.{cql}");
-        applyScripts(dataLoader, "config/cql/migration/", "V*.{cql}");
+        applyScripts(dataLoader, "config/cql/", "*_added_entity_*.cql");
+        applyScripts(dataLoader, "config/cql/migration/", "V*.cql");
     }
 
-    private static void applyScripts(CQLDataLoader dataLoader, String cqlDir, String pattern) throws IOException {
-        try {
-            Path path = Paths.get(ClassLoader.getSystemResource(cqlDir).toURI());
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, pattern)) {
-                for (Path entry : stream) {
-                    String fileName = entry.getFileName().toString();
-                    dataLoader.load(new ClassPathCQLDataSet(cqlDir + fileName, false, false, CASSANDRA_UNIT_KEYSPACE));
-                }
+    private static void applyScripts(CQLDataLoader dataLoader, String cqlDir, String pattern) throws IOException, URISyntaxException {
+        Path path = Paths.get(ClassLoader.getSystemResource(cqlDir).toURI());
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, pattern)) {
+            for (Path entry : stream) {
+                String fileName = entry.getFileName().toString();
+                dataLoader.load(new ClassPathCQLDataSet(cqlDir + fileName, false, false, CASSANDRA_UNIT_KEYSPACE));
             }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
         }
     }
 
