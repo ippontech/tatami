927cca572744189daeaa985191464ec32d35686a
kreinerjm <kreinerjm@hotmail.com>
2016-04-05 17:49:33 -0400

    Searching groups

--- src/main/webapp/scripts/app/account/groups/list/search/search.controller.js
+++ src/main/webapp/scripts/app/account/groups/list/search/search.controller.js
@@ -4,7 +4,7 @@ tatamiJHipsterApp
     $scope.search = {};
     $scope.search = function() {
                 console.log($scope.search.term);
-                $scope.userGroups = SearchService.searchGroups({ query: $scope.search.term });
+                $scope.userGroups = SearchService.query({ term: 'groups', q: $scope.search.term });
                 console.log($scope.userGroups);
             };
 }]);
