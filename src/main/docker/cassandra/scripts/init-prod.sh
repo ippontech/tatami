2f3e69267ab80e5a44baae3ba13e8618cb811e4f
Raphael Brugier <raphael.brugier@gmail.com>
2016-04-05 12:58:44 -0400

    Reflect JHipster changes

--- src/main/docker/cassandra/scripts/init-prod.sh
+++ src/main/docker/cassandra/scripts/init-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 KEYSPACE_NAME=tatamijhipster
-cqlsh -f /cql/create-keyspace.cql $CASSANDRA_CONTACT_POINT
-cqlsh -k $KEYSPACE_NAME -f /cql/create-tables-prod.cql $CASSANDRA_CONTACT_POINT
+cqlsh -f /cql/create-keyspace-prod.cql $CASSANDRA_CONTACT_POINT
+cqlsh -k $KEYSPACE_NAME -f /cql/create-tables.cql $CASSANDRA_CONTACT_POINT
