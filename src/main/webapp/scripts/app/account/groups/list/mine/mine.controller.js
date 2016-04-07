4a78dd4e31646e177329ebacc62edb96585abf8c
kreinerjm <kreinerjm@hotmail.com>
2016-04-07 14:31:38 -0400

    Removed console statements

--- src/main/webapp/scripts/app/account/groups/list/mine/mine.controller.js
+++ src/main/webapp/scripts/app/account/groups/list/mine/mine.controller.js
@@ -1,5 +1,4 @@
 tatamiJHipsterApp
     .controller('GroupsListMineController', ['$scope', 'userGroups', function($scope, userGroups) {
         $scope.userGroups = userGroups;
-        console.log($scope.userGroups);
     }]);
