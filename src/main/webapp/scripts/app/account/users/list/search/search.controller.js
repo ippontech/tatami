c92badadac2f8405c9730f454fc18b16527f9deb
kreinerjm <kreinerjm@hotmail.com>
2016-04-07 14:31:38 -0400

    Removed console statements

--- src/main/webapp/scripts/app/account/users/list/search/search.controller.js
+++ src/main/webapp/scripts/app/account/users/list/search/search.controller.js
@@ -3,13 +3,11 @@ tatamiJHipsterApp
 
     $scope.search = {};
     $scope.search = function() {
-        console.log($scope.search.term);
         if($scope.search.term.length > 0) {
             $scope.usersList = SearchService.query({ term: 'users', q: $scope.search.term });
         }
         else{
             $scope.usersList = null;
         };
-        console.log($scope.usersList);
     };
 }]);
