965f4f81f3cb08de2d0216fc7fcd32c4026bcfd9
Kenny Hegeland <khegeland@ipponusa.com>
2016-04-13 10:54:21 -0400

    Speed up compiling directive so that we only watch the content once

--- mobile/www/app/tatamiApp.js
+++ mobile/www/app/tatamiApp.js
@@ -21,9 +21,7 @@
             // for form inputs)
 
             if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                 cordova.plugins.Keyboard.disableScroll(true);
-
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
@@ -78,7 +76,7 @@
 
             directive.$inject = ['scope', 'element', 'attrs'];
             function directive(scope, element, attrs) {
-                scope.$watch(
+                var ensureCompileRunsOnce = scope.$watch(
                     function(scope) {
                         return scope.$eval(attrs.compile);
                     },
@@ -86,6 +84,8 @@
                         element.html(value);
 
                         $compile(element.contents())(scope);
+
+                        ensureCompileRunsOnce();
                     }
                 );
             }
