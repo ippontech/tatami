d4c03bdeddcada59d11d63952bbe1c046c6cb1f2
Kenny Hegeland <khegeland@ipponusa.com>
2016-04-14 16:38:17 -0400

    Add view and controller for the tag state

---
+++ mobile/www/app/shared/state/tag/tag.controller.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    angular.module('tatami')
+        .controller('TagCtrl', tagCtrl);
+
+    tagCtrl.$inject = ['tag', 'statuses', 'currentUser', 'TatamiStatusRefresherService'];
+    function tagCtrl(tag, statuses, currentUser, TatamiStatusRefresherService) {
+        var vm = this;
+        vm.tag = tag;
+        vm.statuses = statuses;
+        vm.currentUser = currentUser;
+        vm.getNewStatuses = getNewStatuses;
+        vm.getOldStatuses = getOldStatuses;
+
+        function getNewStatuses() {
+            return TatamiStatusRefresherService.refreshTagTimeline(vm.tag);
+        }
+
+        getOldStatuses.$inject = ['finalStatus'];
+        function getOldStatuses(finalStatus) {
+            return TatamiStatusRefresherService.getOldTags(finalStatus)
+        }
+    }
+})();
