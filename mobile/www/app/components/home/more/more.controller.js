77992d63749cf3302e36e5c6b69835721c451026
Kenny Hegeland <khegeland@ipponusa.com>
2016-04-18 15:37:50 -0400

    Add form to allow user to change the server endpoint being used

--- mobile/www/app/components/home/more/more.controller.js
+++ mobile/www/app/components/home/more/more.controller.js
@@ -4,12 +4,29 @@
     angular.module('tatami')
         .controller('MoreController', moreController);
 
-    moreController.$inject = ['$scope', '$state', '$localStorage', '$ionicHistory', '$translate'];
-    function moreController($scope, $state, $localStorage, $ionicHistory, $translate) {
+    moreController.$inject = [
+        '$scope',
+        '$state',
+        '$http',
+        '$localStorage',
+        '$ionicHistory',
+        '$translate',
+        '$ionicPopup',
+        'PathService',
+        'TatamiEndpoint'
+    ];
+    function moreController($scope, $state, $http, $localStorage, $ionicHistory, $translate, $ionicPopup, PathService, TatamiEndpoint) {
         var vm = this;
+
+        vm.endpoint = TatamiEndpoint.getEndpoint().url || TatamiEndpoint.getDefaultEndpoint().url;
+        vm.attempted = false;
+        vm.success = true;
 
         vm.logout = logout;
         vm.goToCompanyTimeline = goToCompanyTimeline;
+        vm.updateEndpoint = updateEndpoint;
+        vm.isDefaultEndpoint = isDefaultEndpoint;
+        vm.useDefaultEndpoint = useDefaultEndpoint;
 
         vm.language = $localStorage.get('language');
 
@@ -36,11 +53,47 @@
             $localStorage.clear();
             $localStorage.set('language', vm.language);
             $ionicHistory.clearCache();
+            vm.attempted = false;
             $state.go('login');
         }
 
         function goToCompanyTimeline() {
             $state.go('company');
+        }
+
+        function updateEndpoint() {
+            TatamiEndpoint.setEndpoint(vm.endpoint);
+            $http({
+                url: PathService.buildPath('/tatami/rest/client/id'),
+                method: 'GET'
+            }).then(success, error);
+        }
+
+        function success(result) {
+            vm.success = true;
+            vm.attempted = true;
+
+            var alertPopup = $ionicPopup.alert({
+                title: '<span translate="more.endpoint.authenticate.title"></span>',
+                template: '<span translate="more.endpoint.authenticate.body"></span>'
+            });
+
+            alertPopup.then(vm.logout);
+        }
+
+        function error(result) {
+            vm.success = false;
+            vm.attempted = true;
+            TatamiEndpoint.reset();
+        }
+
+        function isDefaultEndpoint() {
+            return TatamiEndpoint.getEndpoint().url === vm.endpoint;
+        }
+
+        function useDefaultEndpoint() {
+            vm.endpoint = TatamiEndpoint.getDefault().url;
+            updateEndpoint();
         }
     }
 })();
