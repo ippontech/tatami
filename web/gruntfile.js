991420292a972a85f690f667045f4267d3c4958d
Théo LEBRUN <mib.ftw@gmail.com>
2016-07-13 16:32:37 -0400

    Removed wrong grunt task

--- web/gruntfile.js
+++ web/gruntfile.js
@@ -240,7 +240,6 @@ module.exports = function(grunt) {
         }
 
    } );
-    grunt.loadNpmTasks('grunt-');
     grunt.loadNpmTasks('grunt-template');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
