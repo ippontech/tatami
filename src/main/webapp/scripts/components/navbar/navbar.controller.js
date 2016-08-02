70c593a3f5ea7c27395ae1f45e370f1acb9eec36
matthieudelafourniere <matthieudelafourniere@gmail.com>
2016-08-02 11:14:48 -0400

    navbar controller cleanup

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -18,15 +18,12 @@ tatamiJHipsterApp
 
             return SearchService.get({ term: 'all', q: $scope.search.term }).$promise.then(function(result) {
                 if(angular.isDefined(result.groups[0])) {
-                    console.log("groups is true");
                     result.groups[0].firstGroup = true;
                 }
                 if(angular.isDefined(result.tags[0])) {
-                    console.log("tags is true");
                     result.tags[0].firstTag = true;
                 }
                 if(angular.isDefined(result.users[0])) {
-                    console.log("users is true");
                     result.users[0].firstUser = true;
                 }
                 $scope.allSearch = result.groups.concat(result.users.concat(result.tags));
@@ -35,7 +32,6 @@ tatamiJHipsterApp
         };
 
         $scope.goToPage = function($item) {
-            console.log($item);
             $scope.search.term = '';
             if($item.groupId) {
                 $scope.$state.go('groupStatus', { groupId: $item.groupId });
