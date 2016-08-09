4f6fd29091cee4008da3bfee72f295cbe81a042d
Emily Klein <Kleiner617@gmail.com>
2016-08-09 10:42:20 -0400

    display all users in user management by infinite scroll

--- src/main/webapp/scripts/app/admin/user-management/user-management.controller.js
+++ src/main/webapp/scripts/app/admin/user-management/user-management.controller.js
@@ -4,6 +4,9 @@ tatamiJHipsterApp
     .controller('UserManagementController', function ($scope, UserService, ParseLinks, Language) {
         $scope.users = [];
         $scope.authorities = ["ROLE_USER", "ROLE_ADMIN"];
+        $scope.busy = false;
+        $scope.pagination = 0;
+
         Language.getAll().then(function (languages) {
             $scope.languages = languages;
         });
@@ -12,6 +15,7 @@ tatamiJHipsterApp
         $scope.loadAll = function () {
             UserService.query({}, function (result) {
                 $scope.users = result;
+                $scope.pagination = $scope.users.length;
             });
         };
 
@@ -28,6 +32,37 @@ tatamiJHipsterApp
                 $scope.clear();
             });
         };
+
+        $scope.getMoreUsers = function (){
+            if($scope.busy || $scope.end) {
+                return;
+            }
+
+            $scope.busy = true;
+
+            UserService.query({ finish: $scope.finish, pagination: $scope.pagination }, $scope.loadMoreUsers);
+        }
+
+        $scope.loadMoreUsers = function (results){
+
+            if(results.length === 0){
+                $scope.end = true; // reached end of list
+                return;
+            }
+
+            $scope.pagination += results.length;
+
+            for(var i = 0; i < results.length; i++) {
+                //This is a super hacky fix, but not sure of a better way to fix it.
+                if($scope.users[$scope.users.length-1].id != results[i].id){
+                    $scope.users.push(results[i]);
+                }
+            }
+
+            $scope.finish = results[results.length - 1].id;
+            $scope.busy = false;
+        };
+
 
         $scope.clear = function () {
             $scope.user = {
